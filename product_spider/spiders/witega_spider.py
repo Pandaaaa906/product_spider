a036097f94ef613c8b8a3830aafb66ce1b961ed9
Pandaaaa906 <ye.pandaaaa906@gmail.com>
2021-01-05 16:00:03 +0800

    modify witega

--- product_spider/spiders/witega_spider.py
+++ product_spider/spiders/witega_spider.py
@@ -26,7 +26,7 @@ class WitegaSpider(BaseSpider):
         d = {
             "brand": "Witega",
             "cat_no": response.xpath(tmp.format("order-no")).get(),
-            "en_name": response.xpath('//div[@class="summary entry-summary"]/h1/text()').get(),
+            "en_name": ''.join(response.xpath('//div[@class="summary entry-summary"]/h2//text()').getall()),
             "cas": response.xpath(tmp.format("cas-number")).get(),
             'info1': ''.join(response.xpath('//h5//text()').getall()) or None,
             'prd_url': response.url,
