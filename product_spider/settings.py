db7085a6bf8049f1181d0e8e6c995b08e0628c23
Pandaaaa906 <ye.pandaaaa906@gmail.com>
2021-02-04 22:07:42 +0800

    hongmeng

--- product_spider/settings.py
+++ product_spider/settings.py
@@ -36,7 +36,9 @@ DATABASE = {"engine": DATABASE_ENGINE,
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 # USER_AGENT = 'product_spider (+http://www.yourdomain.com)'
-USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36"
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " \
+             "AppleWebKit/537.36 (KHTML, like Gecko) " \
+             "Chrome/88.0.4324.146 Safari/537.36"
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
