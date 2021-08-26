699f50bd153b728c2788a26a00c058aad99ccfbd
Pandaaaa906 <ye.pandaaaa906@gmail.com>
2021-08-26 14:54:07 +0800

    pep 848

--- product_spider/spiders/tenovapharama_spider.py
+++ product_spider/spiders/tenovapharama_spider.py
@@ -6,16 +6,15 @@ import re
 from product_spider.utils.spider_mixin import BaseSpider
 
 
-
 class TenovapharmalSpider(BaseSpider):
     name = "tenovapharma"
     allow_domain = ["tenovapharma.com"]
     start_urls = ["https://tenovapharma.com/collections/all/", ]
     base_url = "https://tenovapharma.com/collections/all"
 
-
     def parse(self, response):
-        pro_list = response.xpath('//a[@title="Products"]/following-sibling::ul//li[@role="menuitem"]/a[@title and not(following-sibling::ul)]')
+        pro_list = response.xpath(
+            '//a[@title="Products"]/following-sibling::ul//li[@role="menuitem"]/a[@title and not(following-sibling::ul)]')
         for pro in pro_list:
             url = pro.xpath("./@href").get()
             parent = pro.xpath("./@title").get()
@@ -26,13 +25,11 @@ class TenovapharmalSpider(BaseSpider):
                 meta={'parent': parent}
             )
 
-
-
     def parse_list(self, response):
         parent = response.meta.get('parent')
         urls = response.xpath("//div[@class='relative']/a/@href").getall()
         for url in urls:
-            url = urljoin(self.base_url,url)
+            url = urljoin(self.base_url, url)
             yield Request(
                 url=url,
                 callback=self.parse_detial,
@@ -41,7 +38,7 @@ class TenovapharmalSpider(BaseSpider):
 
         next_url = response.xpath('//div[@class="pagination"]//a[@class="next"]/@href').get()
         if next_url:
-            next_url = urljoin(self.base_url,next_url)
+            next_url = urljoin(self.base_url, next_url)
             yield Request(
                 url=next_url,
                 callback=self.parse,
@@ -51,21 +48,21 @@ class TenovapharmalSpider(BaseSpider):
     def parse_detial(self, response):
         parent = response.meta.get('parent')
         cat_no = response.xpath("//span[@class='variant-sku']//text()").get()
-        cat_no = first(re.findall(r'SKU:(.+)-',cat_no), None)
-        d={
-            "brand" : self.name,
-            "parent" : parent,
-            "en_name" : response.xpath("//h1[@class='product-header']/text()").get(),
-            "cat_no" : cat_no,
-            "prd_url" : response.url,
-            "mf" : response.xpath('//td[contains(text(), "Molecular Formula:")]/following-sibling::td/text()').get(),
-            "mw" : response.xpath('//td[contains(text(), "Molecular Weight:")]/following-sibling::td/text()').get(),
-            "cas" : response.xpath('//td[contains(text(), "CAS Number:")]/following-sibling::td/text()').get(),
-            "smiles" : response.xpath('//td[contains(text(), "SMILES:")]/following-sibling::td/text()').get(),
-            "purity" : response.xpath('//td[contains(text(), "Purity (HPLC):")]/following-sibling::td/text()').get(),
-            "info1" : response.xpath('//td[contains(text(), "Synonyms:")]/following-sibling::td/text()').get(),
-            "info2" : response.xpath('//td[contains(text(), "Storage Conditions:")]/following-sibling::td/text()').get(),
-            "img_url" : (m:=response.xpath('//noscript/img/@src').get()) and urljoin(response.url, m),
+        cat_no = first(re.findall(r'SKU:(.+)-', cat_no), None)
+        d = {
+            "brand": self.name,
+            "parent": parent,
+            "en_name": response.xpath("//h1[@class='product-header']/text()").get(),
+            "cat_no": cat_no,
+            "prd_url": response.url,
+            "mf": response.xpath('//td[contains(text(), "Molecular Formula:")]/following-sibling::td/text()').get(),
+            "mw": response.xpath('//td[contains(text(), "Molecular Weight:")]/following-sibling::td/text()').get(),
+            "cas": response.xpath('//td[contains(text(), "CAS Number:")]/following-sibling::td/text()').get(),
+            "smiles": response.xpath('//td[contains(text(), "SMILES:")]/following-sibling::td/text()').get(),
+            "purity": response.xpath('//td[contains(text(), "Purity (HPLC):")]/following-sibling::td/text()').get(),
+            "info1": response.xpath('//td[contains(text(), "Synonyms:")]/following-sibling::td/text()').get(),
+            "info2": response.xpath('//td[contains(text(), "Storage Conditions:")]/following-sibling::td/text()').get(),
+            "img_url": (m := response.xpath('//noscript/img/@src').get()) and urljoin(response.url, m),
         }
         yield RawData(**d)
 
@@ -74,12 +71,10 @@ class TenovapharmalSpider(BaseSpider):
             package, price = row.split("-")
             price = price.replace("$", '')
             dd = {
-                "brand" : self.name,
-                "cat_no" : cat_no,
-                "package" : package,
-                "currency" : "USD",
+                "brand": self.name,
+                "cat_no": cat_no,
+                "package": package,
+                "currency": "USD",
                 "price": price
             }
             yield ProductPackage(**dd)
-
-
