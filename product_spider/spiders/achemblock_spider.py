1a68dc7b07d388ea32ea49e6dd2561ff1964f056
Pandaaaa906 <ye.pandaaaa906@gmail.com>
2021-08-27 18:30:11 +0800

    pep 848

--- product_spider/spiders/achemblock_spider.py
+++ product_spider/spiders/achemblock_spider.py
@@ -8,7 +8,7 @@ class AchemblockSpider(BaseSpider):
     allow_domain = ["achemblock.com"]
     start_urls = ["https://www.achemblock.com/products.html", ]
 
-    def parse(self, response):  ##分类
+    def parse(self, response, **kwargs):  # 分类
         urls = response.xpath("//li[@class='level1 nav-1-1 first']/a/@href")
         for url in urls:
             yield scrapy.Request(
@@ -16,7 +16,7 @@ class AchemblockSpider(BaseSpider):
                 callback=self.parse_list
             )
 
-    def parse_list(self, response):  ##获取详情页url和next_url
+    def parse_list(self, response):  # 获取详情页url和next_url
         rows = response.xpath("//div[@class='products wrapper grid products-grid']//li")
         for row in rows:
             href = row.xpath(".//a/@href").get()
@@ -34,32 +34,22 @@ class AchemblockSpider(BaseSpider):
     def parse_detail(self, response):
         parent = response.xpath("//div[@class='breadcrumbs']//li[last()-1]/a/text()").get()
         en_name = response.xpath("//div[@class='breadcrumbs']//li[last()]/strong/text()").get()
+        tmp = "//div[@class='product-info-custom']//span[contains(text(), {!r}})]//following-sibling::span//text()"
         d = {
             "brand": self.name,
             "parent": parent,
-            "cat_no": response.xpath(
-                "//div[@class='product-info-custom']//span[contains(text(), 'Cat ID')]//following-sibling::span//text()").get(),
+            "cat_no": response.xpath(tmp.format('Cat ID')).get(),
             "en_name": en_name,
-            "cas": response.xpath(
-                "//div[@class='product-info-custom']//span[contains(text(), 'CAS')]//following-sibling::span//text()").get(),
-            "smiles": response.xpath(
-                "//div[@class='product-info-custom']//span[contains(text(), 'Smiles')]//following-sibling::span//text()").get(),
-            "mf": response.xpath(
-                "//div[@class='product-info-custom']//span[contains(text(), 'Formula:')]//following-sibling::span//text()").get(),
-            "mw": response.xpath(
-                "//div[@class='product-info-custom']//span[contains(text(), 'FW:')]//following-sibling::span//text()").get(),
-            "purity": response.xpath(
-                "//div[@class='product-info-custom']//span[contains(text(), 'Purity:')]//following-sibling::span//text()").get(),
-            "appearance": response.xpath(
-                "//table[@class='data table additional-attributes']//th[contains(text(), 'Appearances')]//following-sibling::td//text()").get(),
+            "cas": response.xpath(tmp.format('CAS')).get(),
+            "smiles": response.xpath(tmp.format('Smiles')).get(),
+            "mf": response.xpath(tmp.format('Formula:')).get(),
+            "mw": response.xpath(tmp.format('FW:')).get(),
+            "purity": response.xpath(tmp.format('Purity:')).get(),
+            "appearance": response.xpath("//th[contains(text(), 'Appearances')]//following-sibling::td//text()").get(),
             "img_url": response.xpath("//div[@class='gallery-placeholder _block-content-loading']/img/@src").get(),
             "prd_url": response.url,
-            "info1": response.xpath(
-                "//div[@class='product-info-custom']//span[contains(text(), 'IUPAC Name:')]//following-sibling::span//text()").get(),
-            "info2": response.xpath(
-                "//table[@class='data table additional-attributes']//th[contains(text(), 'Storage')]//following-sibling::td//text()").get(),
-            "mdl": response.xpath(
-                "//div[@class='product-info-custom']//span[contains(text(), 'MDL:')]//following-sibling::span//text()").get(),
-
+            "info1": response.xpath(tmp.format('IUPAC Name:')).get(),
+            "info2": response.xpath("//th[contains(text(), 'Storage')]//following-sibling::td//text()").get(),
+            "mdl": response.xpath(tmp.format('MDL:')).get(),
         }
         yield RawData(**d)
