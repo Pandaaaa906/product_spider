ffb7034a19bcb0e045bfe099da282c59ed0d3ce2
Pandaaaa906 <ye.pandaaaa906@gmail.com>
2022-01-10 11:01:14 +0800

    fix tanmo chs_name

--- product_spider/spiders/tanmo_spider.py
+++ product_spider/spiders/tanmo_spider.py
@@ -129,7 +129,7 @@ class TanmoSpider(BaseSpider):
         cat_no = strip(response.xpath(tmp.format("产品编号")).get())
         good_obj = demjson.decode(first(re.findall(r'goodObj: ({[^}]+}),', response.text), '{}'))
 
-        chs_name = strip(response.xpath('//h2[@class="p-right-title"]/text()').get())
+        chs_name = strip(''.join(response.xpath('//h2[@class="p-right-title"]//text()').getall()))
         cas = strip(response.xpath(tmp.format("CAS号")).get())
         stock_info = good_obj.get('number', 0)
         expiry_date = good_obj.get('date', 0)
