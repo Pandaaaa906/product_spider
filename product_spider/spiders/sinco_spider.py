ee81b061b4227502937b5424b8fef573e07fad80
xiantianlang <xtl19734682555>
2022-03-18 15:05:02 +0800

    fix sinco

--- product_spider/spiders/sinco_spider.py
+++ product_spider/spiders/sinco_spider.py
@@ -1,10 +1,9 @@
+import re
 from itertools import chain
 from string import ascii_uppercase
 from urllib.parse import urljoin
-
 from scrapy import Request
-
-from product_spider.items import RawData
+from product_spider.items import RawData, ProductPackage
 from product_spider.utils.spider_mixin import BaseSpider
 
 
@@ -13,7 +12,7 @@ class SincoSpider(BaseSpider):
     start_urls = (f"http://www.sincopharmachem.com/category.asp?c={c}" for c in chain(ascii_uppercase, ('OTHER',)))
     base_url = "http://www.sincopharmachem.com"
 
-    def parse(self, response):
+    def parse(self, response, **kwargs):
         a_nodes = response.xpath('//li[@class="product-category-item"]/a')
         for a in a_nodes:
             url = urljoin(self.base_url, a.xpath('./@href').get())
@@ -24,17 +23,23 @@ class SincoSpider(BaseSpider):
         urls = response.xpath('//div[@class="rm"]/a/@href').extract()
         for url in urls:
             yield Request(url, meta=response.meta, callback=self.detail_parse)
-        next_page = response.xpath('//li[contains(@class,"pro_active")]//following-sibling::li/a/@href').get()
+        next_page = response.xpath("//li[@class='pro_fyq_next']/a/@href").get()
         if next_page:
             yield Request(urljoin(self.base_url, next_page), meta=response.meta, callback=self.list_parse)
 
     def detail_parse(self, response):
         tmp_xpath = '//*[contains(text(), {!r})]/ancestor::td/following-sibling::td//text()'
         tmp_cat_no = response.xpath('//*[contains(text(), "CAT#:")]/text()').get()
+        cat_no = "".join(response.xpath(tmp_xpath.format("CAT#:")).extract()) \
+                 or response.xpath("//*[contains(text(), 'CAT#:')]/following-sibling::td/text()").get() \
+                 or tmp_cat_no.replace('CAT#:', '') \
+                 or re.search(r"(?<=\t)\w\d{5}",
+                              ''.join(response.xpath("//div[@class='pro_det_jj']/p[position()=1]/text()")))
+
         d = {
             "brand": "sinco",
             "parent": response.meta.get('parent'),
-            "cat_no": "".join(response.xpath(tmp_xpath.format("CAT#:")).extract()) or tmp_cat_no.replace('CAT#:', ''),
+            "cat_no": cat_no,
             "cas": "".join(response.xpath(tmp_xpath.format("CAS#:")).extract()),
             "en_name": response.xpath('//div[@class="right pro_det_nr"]/h1/text()').get(),
             "mf": "".join(response.xpath(tmp_xpath.format("M.F")).extract()),
@@ -43,4 +48,15 @@ class SincoSpider(BaseSpider):
             "prd_url": response.url,
         }
         yield RawData(**d)
-
+        packages = response.xpath(tmp_xpath.format("Specification: ")).get() or \
+                   response.xpath(
+                       "//strong[contains(text(), 'Specification: ')]/ancestor::td/following-sibling::td//span/text()").get()
+        if packages:
+            packages = packages.split(',')
+            for package in packages:
+                dd = {
+                    "brand": "sinco",
+                    "cat_no": cat_no,
+                    "package": package
+                }
+                yield ProductPackage(**dd)
