e4b64c4cdb94b8fef56a044883adcc850ad84799
xiantianlang <xtl19734682555>
2022-04-12 10:41:08 +0800

    fix lgc

--- product_spider/spiders/lgc_spider.py
+++ product_spider/spiders/lgc_spider.py
@@ -1,4 +1,5 @@
 import json
+import time
 from urllib.parse import parse_qsl, urlparse
 import scrapy
 from more_itertools.more import first
@@ -35,16 +36,12 @@ class LGCSpider(JsonSpider):
             else:
                 mf = first(mf).replace(' ', '')
 
-            if (cas := prd.get("listCASNumber")) is None:
-                cas = []
-            cas = ''.join(cas)
             package = ''.join(prd.get("uom", '').split())
             d = {
                 "brand": self.name,
                 "cat_no": cat_no,
                 "en_name": en_name,
                 "mf": mf,
-                "cas": cas,
                 "mw": mw,
                 "prd_url": prd_url,
                 "img_url": img_url,
@@ -55,8 +52,15 @@ class LGCSpider(JsonSpider):
                 "package": package,
                 "currency": "USD",
             }
-            yield RawData(**d)
-            yield ProductPackage(**dd)
+            yield scrapy.Request(
+                url=prd_url,
+                callback=self.parse_detail,
+                meta={
+                    "product": d,
+                    "package": dd,
+                }
+            )
+        time.sleep(5)
         current_page_num = int(dict(parse_qsl(urlparse(response.url).query)).get('currentPage', None))
         if current_page_num is not None:
             current_page_num = current_page_num + 1
@@ -64,3 +68,38 @@ class LGCSpider(JsonSpider):
                 url=f"https://www.lgcstandards.com/US/en/lgcwebservices/lgcstandards/products/search?pageSize=100&fields=FULL&sort=code-asc&currentPage={current_page_num}&q=LGC%3A:itemtype:LGCProduct:itemtype:ATCCProduct&country=US&lang=en&defaultB2BUnit=",
                 callback=self.parse
             )
+
+    def parse_detail(self, response):
+        time.sleep(3)
+        d = response.meta.get("product", None)
+        dd = response.meta.get("package", None)
+        info2 = response.xpath("//*[contains(text(), 'Storage Temperature')]/following-sibling::p/text()").get()  # 储存条件
+        shipping_info = response.xpath(
+            "//*[contains(text(), 'Shipping Temperature')]/following-sibling::p/text()").get()
+        smiles = response.xpath("//*[contains(text(), 'SMILES')]/following-sibling::p/text()").get()
+        cas = response.xpath("//*[contains(text(), 'CAS Number')]/following-sibling::p/text()").get()
+        stock_num = response.xpath("//*[@class='orderbar__stock-title in-stock-green']/text()").get()
+        en_name = response.xpath("//*[contains(text(), 'Analyte Name')]/following-sibling::p/text()").get()
+        inchi = response.xpath("//*[contains(text(), 'InChI')]/following-sibling::p/text()").get()
+        iupac = response.xpath("//*[contains(text(), 'IUPAC')]/following-sibling::p/text()").get()
+
+        api_name = ''.join(response.xpath("//*[contains(text(), 'API Family')]/following-sibling::a/text()").getall())
+
+        parent = ''.join(
+            response.xpath("//*[contains(text(), 'Product Categories')]/following-sibling::p//a/text()").getall())
+
+        prd_attrs = json.dumps({
+            "api_name": api_name,
+            "inchi": inchi,
+            "iupac": iupac,
+        })
+        d["parent"] = parent
+        d["info2"] = info2
+        d["cas"] = cas
+        d["en_name"] = en_name
+        d["shipping_info"] = shipping_info
+        d["smiles"] = smiles
+        d["attrs"] = prd_attrs
+        dd["stock_num"] = stock_num
+        yield RawData(**d)
+        yield ProductPackage(**dd)
