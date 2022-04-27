b70b5a2e7f7578c0ea51fd3aee04eb60ba3cda43
xiantianlang <xtl19734682555>
2022-04-27 09:38:03 +0800

    fix aaronchem

--- product_spider/spiders/aaronchem_spider.py
+++ product_spider/spiders/aaronchem_spider.py
@@ -2,7 +2,7 @@ from urllib.parse import urljoin
 
 import scrapy
 from scrapy.spiders import CrawlSpider
-
+from more_itertools import first
 from product_spider.items import RawData, ProductPackage
 
 
@@ -15,6 +15,12 @@ class AaronchemSpider(CrawlSpider):
     custom_settings = {
         'CONCURRENT_REQUESTS_PER_DOMAIN': 2,
     }
+
+    def start_requests(self):
+        yield scrapy.Request(
+            url='https://www.aaronchem.com/product.html?page=1',
+            callback=self.parse
+        )
 
     def parse(self, response, **kwargs):
         nodes = response.xpath('//ul[@class="ul2 "]/li/a')
@@ -45,18 +51,44 @@ class AaronchemSpider(CrawlSpider):
         img_url = response.xpath("//div[@class='detail_img']/img/@src").get()
 
         cat_no = response.xpath("//td[contains(text(), 'Catalog Number')]/following-sibling::td/text()").get()
+        if cat_no:
+            cat_no = first(cat_no.split(), None)
+
+        mdl = response.xpath("//td[contains(text(), 'MDL Number')]/following-sibling::td/text()").get()
+        if mdl:
+            mdl = first(mdl.split(), None)
+        smiles = response.xpath("//td[contains(text(), 'SMILES')]/following-sibling::td/text()").get()
+        if smiles:
+            smiles = first(smiles.split(), None)
+
+        info1 = response.xpath("//td[contains(text(), 'Chemical Name')]/following-sibling::td/text()").get()
+        if info1:
+            info1 = first(info1.split(), None)
+
+        cas = response.xpath("//td[contains(text(), 'CAS Number')]/following-sibling::td/text()").get()
+        if cas:
+            cas = first(cas.split(), None)
+
+        mf = response.xpath("//td[contains(text(), 'Molecular Formula')]/following-sibling::td/text()").get()
+        if mf:
+            mf = first(mf.split(), None)
+
+        mw = response.xpath("//td[contains(text(), 'Molecular Weight')]/following-sibling::td/text()").get()
+        if mw:
+            mw = first(mw.split(), None)
+
         d = {
             "brand": self.name,
             "prd_url": response.url,
             "en_name": response.xpath("//div[@class='detail_des']/h2/text()").get(),
             "img_url": urljoin(self.base_url, img_url),
             "cat_no": cat_no,
-            "mdl": response.xpath("//td[contains(text(), 'MDL Number')]/following-sibling::td/text()").get(),
-            "smiles": response.xpath("//td[contains(text(), 'SMILES')]/following-sibling::td/text()").get(),
-            "info1": response.xpath("//td[contains(text(), 'Chemical Name')]/following-sibling::td/text()").get(),
-            "cas": response.xpath("//td[contains(text(), 'CAS Number')]/following-sibling::td/text()").get(),
-            "mf": response.xpath("//td[contains(text(), 'Molecular Formula')]/following-sibling::td/text()").get(),
-            "mw": response.xpath("//td[contains(text(), 'Molecular Weight')]/following-sibling::td/text()").get(),
+            "mdl": mdl,
+            "smiles": smiles,
+            "info1": info1,
+            "cas": cas,
+            "mf": mf,
+            "mw": mw,
         }
         yield RawData(**d)
 
