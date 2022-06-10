1ad8838f95a015e227d421f7aed25adb67e4faac
xiantianlang <xtl19734682555>
2022-06-10 14:58:04 +0800

    AaronchemSpider add SupplierProduct

--- product_spider/spiders/aaronchem_spider.py
+++ product_spider/spiders/aaronchem_spider.py
@@ -1,9 +1,11 @@
+import json
 from urllib.parse import urljoin
 
 import scrapy
 from scrapy.spiders import CrawlSpider
-from more_itertools import first
-from product_spider.items import RawData, ProductPackage
+from product_spider.items import RawData, ProductPackage, SupplierProduct
+from product_spider.utils.cost import parse_cost
+from product_spider.utils.functions import strip
 
 
 class AaronchemSpider(CrawlSpider):
@@ -24,13 +26,13 @@ class AaronchemSpider(CrawlSpider):
 
     def parse(self, response, **kwargs):
         nodes = response.xpath('//ul[@class="ul2 "]/li/a')
-        for a in nodes:
-            parent = a.xpath('./text()').get()
-            url = a.xpath('./@href').get()
+        for node in nodes:
+            parent = node.xpath('./text()').get()
+            url = node.xpath('./@href').get()
             yield scrapy.Request(url, callback=self.parse_list, meta={'parent': parent})
 
     def parse_list(self, response):
-        parent = response.meta['parent']
+        parent = response.meta.get('parent', None)
         urls = response.xpath("//a[@class='view_detial']/@href").getall()
         for url in urls:
             yield scrapy.Request(
@@ -44,63 +46,74 @@ class AaronchemSpider(CrawlSpider):
             yield scrapy.Request(
                 url=next_url,
                 callback=self.parse_list,
-                meta={"parent": parent}
             )
 
     def parse_detail(self, response):
-        img_url = response.xpath("//div[@class='detail_img']/img/@src").get()
+        parent = response.meta.get("parent", None)
+        img_url = urljoin(self.base_url, response.xpath("//div[@class='detail_img']/img/@src").get())
+        en_name = response.xpath("//div[@class='detail_des']/h2/text()").get()
+        cat_no = strip(response.xpath("//td[contains(text(), 'Catalog Number')]/following-sibling::td/text()").get())
+        mdl = strip(response.xpath("//td[contains(text(), 'MDL Number')]/following-sibling::td/text()").get())
+        smiles = strip(response.xpath("//td[contains(text(), 'SMILES')]/following-sibling::td/text()").get())
+        info1 = strip(response.xpath("//td[contains(text(), 'Chemical Name')]/following-sibling::td/text()").get())
+        cas = strip(response.xpath("//td[contains(text(), 'CAS Number')]/following-sibling::td/text()").get())
+        mf = strip(response.xpath("//td[contains(text(), 'Molecular Formula')]/following-sibling::td/text()").get())
+        mw = strip(response.xpath("//td[contains(text(), 'Molecular Weight')]/following-sibling::td/text()").get())
 
-        cat_no = response.xpath("//td[contains(text(), 'Catalog Number')]/following-sibling::td/text()").get()
-        if cat_no:
-            cat_no = first(cat_no.split(), None)
+        inchl = strip(response.xpath("//td[contains(text(), 'InChI')]/following-sibling::td/text()").get())
+        inchl_key = strip(response.xpath("//td[contains(text(), 'InChI Key')]/following-sibling::td/text()").get())
+        iupac = strip(response.xpath("//td[contains(text(), 'IUPAC Name')]/following-sibling::td/text()").get())
 
-        mdl = response.xpath("//td[contains(text(), 'MDL Number')]/following-sibling::td/text()").get()
-        if mdl:
-            mdl = first(mdl.split(), None)
-        smiles = response.xpath("//td[contains(text(), 'SMILES')]/following-sibling::td/text()").get()
-        if smiles:
-            smiles = first(smiles.split(), None)
-
-        info1 = response.xpath("//td[contains(text(), 'Chemical Name')]/following-sibling::td/text()").get()
-        if info1:
-            info1 = first(info1.split(), None)
-
-        cas = response.xpath("//td[contains(text(), 'CAS Number')]/following-sibling::td/text()").get()
-        if cas:
-            cas = first(cas.split(), None)
-
-        mf = response.xpath("//td[contains(text(), 'Molecular Formula')]/following-sibling::td/text()").get()
-        if mf:
-            mf = first(mf.split(), None)
-
-        mw = response.xpath("//td[contains(text(), 'Molecular Weight')]/following-sibling::td/text()").get()
-        if mw:
-            mw = first(mw.split(), None)
+        prd_attrs = json.dumps({
+            "inchl": inchl,
+            "inchl_key": inchl_key,
+            "iupac": iupac
+        })
 
         d = {
             "brand": self.name,
-            "prd_url": response.url,
-            "en_name": response.xpath("//div[@class='detail_des']/h2/text()").get(),
-            "img_url": urljoin(self.base_url, img_url),
             "cat_no": cat_no,
+            "parent": parent,
+            "en_name": en_name,
             "mdl": mdl,
             "smiles": smiles,
             "info1": info1,
             "cas": cas,
             "mf": mf,
             "mw": mw,
+            "prd_url": response.url,
+            "img_url": img_url,
+            "attrs": prd_attrs,
         }
         yield RawData(**d)
 
         rows = response.xpath("//div[@class='detail']//tr[position()>1]")
         for row in rows:
             price = row.xpath('./td[3]/text()').get()
-            price = price.replace("$", '')
             dd = {
                 "brand": self.name,
                 "cat_no": cat_no,
                 "package": row.xpath('./td[1]/text()').get(),
                 "currency": "USD",
-                "cost": price,
+                "cost": parse_cost(price),
             }
             yield ProductPackage(**dd)
+
+            ddd = {
+                "platform": self.name,
+                "vendor": self.name,
+                "brand": self.name,
+                "parent": d["parent"],
+                "en_name": d["en_name"],
+                "cas": d["cas"],
+                "mf": d["mf"],
+                "mw": d["mw"],
+                'cat_no': d["cat_no"],
+                'package': dd['package'],
+                'cost': dd['cost'],
+                "smiles": d["smiles"],
+                "currency": dd["currency"],
+                "img_url": d["img_url"],
+                "prd_url": response.url,
+            }
+            yield SupplierProduct(**ddd)
