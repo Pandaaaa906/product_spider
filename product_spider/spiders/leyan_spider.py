9efb3798416a4d1a6af8b5ab5fc3b652ad053732
xiantianlang <xtl19734682555>
2022-06-29 16:09:42 +0800

    fixed LeyanSpider add SupplierProduct

--- product_spider/spiders/leyan_spider.py
+++ product_spider/spiders/leyan_spider.py
@@ -2,7 +2,7 @@ from urllib.parse import urljoin
 
 from scrapy import Request
 
-from product_spider.items import RawData, ProductPackage
+from product_spider.items import RawData, ProductPackage, SupplierProduct
 from product_spider.utils.spider_mixin import BaseSpider
 
 
@@ -35,7 +35,7 @@ class LeyanSpider(BaseSpider):
         cat_no = response.xpath('//span[@id="catalogNo"]/text()').get()
         rel_img = response.xpath('//input[@id="image"]/@value').get()
         d = {
-            'brand': self.brand,
+            'brand': self.name,
             'parent': '_'.join(response.xpath('//li[@class="active"]/following-sibling::li/a/text()').getall()),
             'cat_no': cat_no,
             'en_name': response.xpath('//h2/span/text()').get(),
@@ -56,12 +56,30 @@ class LeyanSpider(BaseSpider):
         for row in rows:
             if not (package := row.xpath('./td[@id="packing"]/text()').get()):
                 continue
-            package = {
-                'brand': self.brand,
+            dd = {
+                'brand': self.name,
                 'cat_no': cat_no,
                 'package': package,
                 'cost': row.xpath('./td[@id="money"]/text()').get(),
                 'currency': 'RMB',
                 'stock_num': row.xpath('./td[@id="stock"]/text()').get(),
             }
-            yield ProductPackage(**package)
+
+            ddd = {
+                "platform": self.name,
+                "vendor": self.name,
+                "brand": self.name,
+                "parent": d["parent"],
+                "en_name": d["en_name"],
+                "cas": d["cas"],
+                "mf": d["mf"],
+                "mw": d["mw"],
+                'cat_no': d["cat_no"],
+                'package': dd['package'],
+                'cost': dd['cost'],
+                "currency": dd["currency"],
+                "img_url": d["img_url"],
+                "prd_url": d["prd_url"],
+            }
+            yield ProductPackage(**dd)
+            yield SupplierProduct(**ddd)
