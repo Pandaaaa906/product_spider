dd405df3d0ddd49cb54e1a08309f1aa6c202b60e
xiantianlang <xtl19734682555>
2022-06-29 16:30:13 +0800

    fixed TCISpider add SupplierProduct

--- product_spider/spiders/tci_spider.py
+++ product_spider/spiders/tci_spider.py
@@ -2,7 +2,7 @@ from urllib.parse import urljoin
 
 from scrapy import Request
 
-from product_spider.items import RawData, ProductPackage
+from product_spider.items import RawData, ProductPackage, SupplierProduct
 from product_spider.utils.functions import strip
 from product_spider.utils.spider_mixin import BaseSpider
 
@@ -75,7 +75,7 @@ class TCISpider(BaseSpider):
         rows = response.xpath('//table[@id="PricingTable"]/tbody/tr')
         for row in rows:
             stock_num = strip(row.xpath('./td[3]/text()').get())
-            package = {
+            dd = {
                 'brand': self.brand,
                 'cat_no': cat_no,
                 'package': row.xpath('./td[1]/text()').get(),
@@ -84,4 +84,22 @@ class TCISpider(BaseSpider):
                 'stock_num': stock_num,
                 'currency': 'RMB',
             }
-            yield ProductPackage(**package)
+
+            ddd = {
+                "platform": self.name,
+                "vendor": self.name,
+                "brand": self.name,
+                "parent": d["parent"],
+                "en_name": d["en_name"],
+                "cas": d["cas"],
+                "mf": d["mf"],
+                "mw": d["mw"],
+                'cat_no': d["cat_no"],
+                'package': dd['package'],
+                'cost': dd['cost'],
+                "currency": dd["currency"],
+                "img_url": d["img_url"],
+                "prd_url": d["prd_url"],
+            }
+            yield ProductPackage(**dd)
+            yield SupplierProduct(**ddd)
