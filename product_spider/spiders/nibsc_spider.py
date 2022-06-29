7cd600646cc7ae0c81238c42bace9ed608b11eb4
xiantianlang <xtl19734682555>
2022-06-29 23:22:43 +0800

    fixed NibscPrdSpider add SupplierProduct

--- product_spider/spiders/nibsc_spider.py
+++ product_spider/spiders/nibsc_spider.py
@@ -3,7 +3,7 @@ from urllib.parse import urljoin
 
 import scrapy
 
-from product_spider.items import RawData, ProductPackage
+from product_spider.items import RawData, ProductPackage, SupplierProduct
 from product_spider.utils.spider_mixin import BaseSpider
 
 
@@ -74,5 +74,19 @@ class NibscPrdSpider(BaseSpider):
             "currency": "GBP",
             "attrs": package_attrs,
         }
+
+        ddd = {
+            "platform": self.name,
+            "vendor": self.name,
+            "brand": self.name,
+            "parent": d["parent"],
+            "en_name": d["en_name"],
+            'cat_no': d["cat_no"],
+            'package': dd['package'],
+            'cost': dd['cost'],
+            "currency": dd["currency"],
+            "prd_url": d["prd_url"],
+        }
         yield RawData(**d)
         yield ProductPackage(**dd)
+        yield SupplierProduct(**ddd)
