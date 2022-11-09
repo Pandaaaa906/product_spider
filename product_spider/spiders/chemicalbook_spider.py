872dc38b2ce689d5b97893b2fa41c0320911a6ef
Pandaaaa906 <ye.pandaaaa906@gmail.com>
2022-11-09 16:08:11 +0800

    fix chemicalbook_spider.py

--- product_spider/spiders/chemicalbook_spider.py
+++ product_spider/spiders/chemicalbook_spider.py
@@ -2,6 +2,8 @@ import json
 import logging
 from urllib.parse import urljoin
 import scrapy
+from scrapy import FormRequest
+
 from product_spider.items import SupplierProduct, ChemicalItem
 from product_spider.middlewares.proxy_middlewares import wrap_failed_request
 from product_spider.utils.cost import parse_cost
@@ -38,16 +40,19 @@ class ChemicalBookSpider(BaseSpider):
 
     def parse(self, response, **kwargs):
         catalog_num = response.meta.get("catalog_num")
-        urls = response.xpath("//div[@id='mainDiv']//tr/td[last()]/a/@href").getall()
-        if not urls:
+        a_nodes = response.xpath("//div[@id='mainDiv']//tr/td[1]/a")
+        if not a_nodes:
             logger.warning(f"product urls : {response.url} get urls fail")
             yield wrap_failed_request(response.request)
             return
 
-        for url in urls:
+        for a_node in a_nodes:
+            url = a_node.xpath('./@href').get()
+            cas = a_node.xpath('./text()').get()
             yield scrapy.Request(
                 url=urljoin(self.base_url, url),
                 callback=self.parse_detail,
+                meta={"cas": cas}
             )
         # 翻页
         next_page = response.xpath('//div[@class="page_jp"]/b/following-sibling::a/@href').get()
@@ -63,7 +68,7 @@ class ChemicalBookSpider(BaseSpider):
         warning_title = response.xpath("//div[@class='SearchEmpty']/h2/span/text()").get()
         if warning_title == '根据相关法律法规和政策，此产品禁止销售！':
             return
-        cas = response.xpath(tmp_xpath.format('CAS号:')).get()
+        cas = response.xpath(tmp_xpath.format('CAS号:')).get() or response.meta.get('cas')
         if not cas:
             logger.warning(f"product detail url: {response.url} get cas fail")
             yield wrap_failed_request(response.request)
@@ -115,7 +120,7 @@ class ChemicalBookSpider(BaseSpider):
         chemical_item_attrs = json.dumps({
             "chemical_attrs": chemical_attrs,
             "safety_attrs": safety_attrs,
-            "pacakges": packages,
+            "packages": packages,
         }, ensure_ascii=False)
 
         dddd = {
@@ -126,6 +131,15 @@ class ChemicalBookSpider(BaseSpider):
         }
         yield ChemicalItem(**dddd)
 
+        response.meta['chem'] = d
+        yield from self.parse_supplier(response)
+
+    def parse_supplier(self, response):
+        d = response.meta.get('chem', {})
+        dont_grab = response.meta.get('dont_grab', False)
+        country_index = int(response.meta.get('country_index', 0))
+
+        cb_code = response.xpath('//dt[text()="CBNumber:"]/following-sibling::dd/text()').get() or d['cas']
         nodes = response.xpath("//th[contains(text(), '供应商')]/parent::tr/following-sibling::tr")
         for node in nodes:
             vendor = node.xpath("./td[position()=1]/a/text()").get()
@@ -135,7 +149,7 @@ class ChemicalBookSpider(BaseSpider):
                 "platform": self.name,
                 "vendor": vendor,
                 "vendor_origin": country,
-                "source_id": f"{vendor}_{d['cas']}",
+                "source_id": f"{vendor}_{cb_code}",
                 "brand": self.name,
                 "chs_name": d["chs_name"],
                 "cas": d["cas"],
@@ -145,8 +159,41 @@ class ChemicalBookSpider(BaseSpider):
                 # "cost": dd["cost"],
                 # "currency": dd["currency"],
                 "cat_no": d["cat_no"],
-                "img_url": img_url,
+                "img_url": d["img_url"],
                 "prd_url": response.url,
             }
             yield SupplierProduct(**ddd)
 
+        if not nodes or dont_grab:
+            return
+
+        event_target = response.xpath('//input[@id="__EVENTTARGET"]/@value').get()
+        event_arg = response.xpath('//input[@id="__EVENTARGUMENT"]/@value').get()
+        last_focus = response.xpath('//input[@id="__LASTFOCUS"]/@value').get()
+        view_state = response.xpath('//input[@id="__VIEWSTATE"]/@value').get()
+        view_state_gen = response.xpath('//input[@id="__VIEWSTATEGENERATOR"]/@value').get()
+        kw = response.xpath('//input[@id="kw"]/@value').get()
+
+        countries = response.xpath('//select[@id="SupplierCountDetail"]//option/@value').getall()
+        if len(countries) <= country_index + 1:
+            return
+        country = countries[country_index]
+        yield FormRequest(
+            url=response.url,
+            formdata={
+                '__EVENTTARGET': event_target,
+                '__EVENTARGUMENT': event_arg,
+                '__LASTFOCUS': last_focus,
+                '__VIEWSTATE': view_state,
+                '__VIEWSTATEGENERATOR': view_state_gen,
+                'kw': kw,
+                'q': '',
+                'SupplierCountDetail': country
+            },
+            callback=self.parse_supplier,
+            meta={
+                'dont_grab': len(countries) > country_index + 1,
+                'chem': d,
+                'country_index': country_index + 1
+            }
+        )
