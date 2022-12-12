5caab0db6eaf241dcb9381f06158c367a7cfd357
xiantianlang <xtl19734682555>
2022-12-12 15:52:00 +0800

    fixed spider AaronchemSpider

--- product_spider/spiders/aaronchem_spider.py
+++ product_spider/spiders/aaronchem_spider.py
@@ -3,7 +3,7 @@ from urllib.parse import urljoin
 
 import scrapy
 from scrapy.spiders import CrawlSpider
-from product_spider.items import RawData, ProductPackage, SupplierProduct
+from product_spider.items import RawData, ProductPackage, SupplierProduct, RawSupplierQuotation
 from product_spider.utils.cost import parse_cost
 from product_spider.utils.functions import strip
 
@@ -103,6 +103,7 @@ class AaronchemSpider(CrawlSpider):
                 "platform": self.name,
                 "vendor": self.name,
                 "brand": self.name,
+                "source_id": f'{self.name}_{d["cat_no"]}_{dd["package"]}',
                 "parent": d["parent"],
                 "en_name": d["en_name"],
                 "cas": d["cas"],
@@ -116,4 +117,17 @@ class AaronchemSpider(CrawlSpider):
                 "img_url": d["img_url"],
                 "prd_url": response.url,
             }
+            dddd = {
+                "platform": self.name,
+                "vendor": self.name,
+                "brand": self.name,
+                "source_id": f'{self.name}_{d["cat_no"]}',
+                'cat_no': d["cat_no"],
+                'package': dd['package'],
+                'discount_price': dd['cost'],
+                'price': dd['cost'],
+                'cas': d["cas"],
+                'currency': dd["currency"],
+            }
             yield SupplierProduct(**ddd)
+            yield RawSupplierQuotation(**dddd)
