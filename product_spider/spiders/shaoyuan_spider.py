470b39be9df98e071e64dbcfc87db3e93a68b8fe
Pandaaaa906 <ye.pandaaaa906@gmail.com>
2023-07-24 18:25:54 +0800

    fix shaoyuan_spider.py using goodsid

--- product_spider/spiders/shaoyuan_spider.py
+++ product_spider/spiders/shaoyuan_spider.py
@@ -1,3 +1,4 @@
+import re
 from urllib.parse import urljoin
 
 from scrapy import Request
@@ -9,30 +10,21 @@ from product_spider.utils.spider_mixin import BaseSpider
 
 class ShaoyuanSpider(BaseSpider):
     name = "shaoyuan"
-    start_urls = ["http://www.shao-yuan.com/cn/productlist_catalogid_205.html", ]
+    # start_urls = ["http://www.shao-yuan.com/cn/productlist_catalogid_205.html", ]
     base_url = "http://www.shao-yuan.com/"
     brand = '韶远'
 
-    def parse(self, response, **kwargs):
-        a_nodes = response.xpath('//div[@class="r_a_b_next_ladder"]//li/a')
-        parent = response.meta.get('parent', '')
-        for a in a_nodes:
-            sub_parent = a.xpath('./text()').get()
-            rel_url = a.xpath('./@href').get()
-            yield Request(urljoin(self.base_url, rel_url), callback=self.parse,
-                          meta={'parent': f'{parent}_{sub_parent}' if parent else sub_parent}
-                          )
-
-        rel_urls = response.xpath('//div[not(@class)]/a[not(@target)]/@href').getall()
-        for rel_url in rel_urls:
-            yield Request(urljoin(self.base_url, rel_url), callback=self.parse_detail, meta={'parent': parent})
-
-        next_page = response.xpath('//a[text()="下一页"]/@href').get()
-        if next_page:
-            yield Request(urljoin(self.base_url, next_page), self.parse, meta={'parent': parent})
+    def start_requests(self):
+        _id = 1
+        while _id < 600000:
+            yield Request(
+                f"http://www.shao-yuan.com/cn/productview_goodsid_{_id}.html",
+                callback=self.parse_detail,
+            )
+            _id += 1
 
     def parse_detail(self, response):
-        tmp = '//li[contains(text(), {!r})]/text()'
+        tmp = '//li[contains(text(), {!r})]//text()'
         func = lambda res, t: res.xpath(tmp.format(t)).get('').lstrip(t) or None
         img_rel = response.xpath('//td/img/@src').get()
 
@@ -42,7 +34,7 @@ class ShaoyuanSpider(BaseSpider):
         d = {
             'brand': self.brand,
             'cat_no': cat_no,
-            'parent': response.meta.get('parent'),
+            'parent': strip(re.sub(r'\s+', ' ', func(response, '产品分类： ') or '')),
             'en_name': strip(response.xpath('//h2/text()[1]').get()),
             'chs_name': strip(response.xpath('//h2/text()[2]').get()),
             'cas': func(response, 'CAS号：'),
@@ -54,13 +46,13 @@ class ShaoyuanSpider(BaseSpider):
             'info4': response.xpath('//tr[@id][1]/td[5]/text()').get(),
             'stock_info': response.xpath('//tr[@id][1]/td[8]/text()').get(),
 
-            'img_url': img_rel and urljoin(self.base_url, img_rel),
+            'img_url': img_rel and urljoin(response.url, img_rel),
             'prd_url': response.url,
         }
         yield RawData(**d)
 
         for tr in response.xpath('//tr[@id]'):
-            d_package = {
+            dd = {
                 'brand': self.brand,
                 'cat_no': cat_no,
                 'package': tr.xpath('./td[4]/text()').get(),
@@ -68,6 +60,6 @@ class ShaoyuanSpider(BaseSpider):
                 'currency': 'RMB',
                 'delivery_time': tr.xpath('./td[8]/text()').get(),
             }
-            if d_package['package'] == 'bulk':
+            if dd['package'] == 'bulk':
                 continue
-            yield ProductPackage(**d_package)
+            yield ProductPackage(**dd)
