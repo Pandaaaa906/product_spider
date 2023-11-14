8ff91de55cf45b80b081b3b245dc5ff3c3581941
Pandaaaa906 <ye.pandaaaa906@gmail.com>
2023-11-14 10:54:11 +0800

    fix cdn

--- product_spider/spiders/cdn_spider.py
+++ product_spider/spiders/cdn_spider.py
@@ -5,19 +5,19 @@ from urllib.parse import urljoin
 from scrapy import Request
 
 from product_spider.items import RawData, ProductPackage, SupplierProduct, RawSupplierQuotation
-from product_spider.utils.functions import strip, first
+from product_spider.utils.functions import strip, first, dumps
 from product_spider.utils.spider_mixin import BaseSpider
 
 
 class CDNPrdSpider(BaseSpider):
     name = 'cdn'
-    brand = 'cdn'
     base_url = "https://cdnisotopes.com/"
     start_urls = [
-        "https://cdnisotopes.com/nf/alphabetlist/view/list/?char=ALL&limit=50", ]
+        "https://cdnisotopes.com/nf/alphabetlist/view/list/?char=ALL&limit=50",
+    ]
 
     def parse(self, response, *args, **kwargs):
-        urls = response.xpath('//ol[@id="products-list"]/li/div[@class="col-11"]/a/@href').extract()
+        urls = response.xpath('//ol[@id="products-list"]/li/div[@class="col-11"]/a/@href').getall()
         for url in urls:
             yield Request(urljoin(self.base_url, url), callback=self.detail_parse)
         next_page = response.xpath('//div[@class="pages"]//li[last()]/a/@href').get()
@@ -29,23 +29,16 @@ class CDNPrdSpider(BaseSpider):
         tmp = '//th[contains(text(),{0!r})]/following-sibling::td/descendant-or-self::text()'
         img_url = response.xpath('//th[contains(text(),"Structure")]/following-sibling::td/img/@src').get()
         cat_no = strip(response.xpath(tmp.format("Product No.")).get())
-        feature = response.xpath(tmp.format('Isotopic Enrichment')).get()  # 特性值
-        function_group = response.xpath(tmp.format("Functional Groups")).get()  # 官能团
 
-        stability = response.xpath(tmp.format('Stability')).get()  # 稳定状态
-        danger_desc = response.xpath(tmp.format("Shipping Hazards")).get()  # 危险标识
-        prd_attrs = json.dumps({
-            "feature": feature,
-            "danger_desc": danger_desc,
-        })
-
-        package_attrs = json.dumps({
-            "function_group": function_group,
-            "stability": stability,
-        })
+        prd_attrs = {
+            "feature": response.xpath(tmp.format('Isotopic Enrichment')).get(),  # 特性值
+            "danger_desc":  response.xpath(tmp.format("Shipping Hazards")).get(),  # 危险标识
+            "function_group": response.xpath(tmp.format("Functional Groups")).get(),  # 官能团
+            "stability": response.xpath(tmp.format('Stability')).get(),  # 稳定状态
+        }
 
         d = {
-            "brand": self.brand,
+            "brand": self.name,
             "cat_no": cat_no,
             "parent": response.xpath(tmp.format("Category")).get(),
             "info1": "".join(response.xpath(tmp.format("Synonym(s)")).extract()),
@@ -58,9 +51,27 @@ class CDNPrdSpider(BaseSpider):
             "stock_info": response.xpath(
                 '//table[@id="product-matrix"]//td[@class="unit-price"]/text()').get(),
             "info2": response.xpath(tmp.format("Storage Conditions")).get(),  # 储存条件
-            "attrs": prd_attrs,
+            "attrs": dumps(prd_attrs),
             "prd_url": response.url,
         }
+        yield RawData(**d)
+
+        ddd = {
+            "platform": self.name,
+            "vendor": self.name,
+            "brand": self.name,
+            "source_id": f'{self.name}_{d["cat_no"]}',
+            "parent": d["parent"],
+            "en_name": d["en_name"],
+            "cas": d["cas"],
+            "mf": d["mf"],
+            "mw": d["mw"],
+            'cat_no': d["cat_no"],
+            "img_url": d["img_url"],
+            "prd_url": d["prd_url"],
+        }
+
+        yield SupplierProduct(**ddd)
 
         matrix = first(re.findall(r'var matrixChildrenProducts = ({.+});', response.text), None)
         if not matrix:
@@ -72,32 +83,13 @@ class CDNPrdSpider(BaseSpider):
                 continue
             package = sku.replace(f'{cat_no}-', '')
             dd = {
-                'brand': self.brand,
+                'brand': self.name,
                 'cat_no': cat_no,
                 'cat_no_unit': sku,
                 'package': strip(package),
                 'cost': str(item.get('price')),
                 'currency': 'USD',
-                'delivery_time': 'In-stock' if item.get('is_in_stock') else None,
-                "attrs": package_attrs,
-            }
-
-            ddd = {
-                "platform": self.name,
-                "vendor": self.name,
-                "brand": self.name,
-                "source_id": f'{self.name}_{d["cat_no"]}_{dd["package"]}',
-                "parent": d["parent"],
-                "en_name": d["en_name"],
-                "cas": d["cas"],
-                "mf": d["mf"],
-                "mw": d["mw"],
-                'cat_no': d["cat_no"],
-                'package': dd['package'],
-                'cost': dd['cost'],
-                "currency": dd["currency"],
-                "img_url": d["img_url"],
-                "prd_url": d["prd_url"],
+                'delivery_time': 'in-stock' if item.get('is_in_stock') else None,
             }
             dddd = {
                 "platform": self.name,
@@ -112,7 +104,5 @@ class CDNPrdSpider(BaseSpider):
                 'currency': dd["currency"],
             }
 
-            yield RawData(**d)
             yield ProductPackage(**dd)
-            yield SupplierProduct(**ddd)
             yield RawSupplierQuotation(**dddd)
