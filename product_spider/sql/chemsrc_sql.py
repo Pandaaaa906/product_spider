5c84dc7a47ec2ba23635bcd2d5b26f5390f63694
Pandaaaa906 <ye.pandaaaa906@gmail.com>
2024-03-04 15:01:32 +0800

    add chemsrc_spider.py

--- product_spider/sql/chemsrc_sql.py
+++ product_spider/sql/chemsrc_sql.py
@@ -6,7 +6,7 @@ distinct cas
 FROM chemhostv2.mod_product_product prd
 WHERE prd.brand_name = 'cato'
 AND prd.cas not ilike all(array['n/a', 'na'])
-AND NOT EXISTS (SELECT 1 FROM "public"."chemsrcchemical" t where t.cas = prd.cas and t.modify_date <= %s)
+AND NOT EXISTS (SELECT 1 FROM "public"."chemsrcchemical" t where t.cas = prd.cas and t.modify_date >= %s)
 
 ORDER BY prd.cas
 
