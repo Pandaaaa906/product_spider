f96976fddfb85423fad84c64369cb3f32c572949
Pandaaaa906 <ye.pandaaaa906@gmail.com>
2024-03-06 12:16:01 +0800

    improve sql_fetch_cas

--- product_spider/sql/chemsrc_sql.py
+++ product_spider/sql/chemsrc_sql.py
@@ -2,12 +2,12 @@
 
 sql_fetch_cas = """
 SELECT
-distinct cas
+cas
 FROM chemhostv2.mod_product_product prd
 WHERE prd.brand_name = 'cato'
-AND prd.cas not ilike all(array['n/a', 'na'])
+AND prd.cas ~ '\d+-\d+-\d'
 AND NOT EXISTS (SELECT 1 FROM "public"."chemsrcchemical" t where t.cas = prd.cas and t.modify_date >= %s)
-
-ORDER BY prd.cas
+GROUP BY prd.cas
+ORDER BY length(prd.cas), prd.cas
 
 """
