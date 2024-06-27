43b8db25a24aa5813a00325368998bcc1197c7cd
Pandaaaa906 <ye.pandaaaa906@gmail.com>
2024-06-27 17:54:50 +0800

    fix bp_spider.py

--- product_spider/spiders/bp_spider.py
+++ product_spider/spiders/bp_spider.py
@@ -6,34 +6,29 @@ import re
 from product_spider.items import RawData, ProductPackage, SupplierProduct, RawSupplierQuotation
 from product_spider.utils.spider_mixin import BaseSpider
 
+
 class BPSpider(BaseSpider):
     name = "bp"
-    start_urls = ["https://www.pharmacopoeia.com/Catalogue/Products", ]
+    start_urls = ["https://www.pharmacopoeia.com/shop/products", ]
     base_url = "https://www.pharmacopoeia.com/"
 
     def parse(self, response, **kwargs):
-        for url in self.start_urls:
-            yield Request(url, callback=self.dummy_parse)
-
-    def dummy_parse(self, response):
-        time.sleep(3)
-        rel_urls = response.xpath('//table[@class="product-table"]/tbody/tr/td[3]/a/@href').getall()
+        rel_urls = response.xpath('//ul[@class="spl-list"]//div/a/@href').getall()
         for rel_url in rel_urls:
-            yield Request(urljoin(self.base_url, rel_url), callback=self.parse_detail)
+            yield Request(urljoin(response.url, rel_url), callback=self.parse_detail)
 
-        next_page = response.xpath('//div[@class="pagination"]//li[@class="active"]/following-sibling::li[1]/a/@href') \
+        next_page = response.xpath('//div[@class="pagination"]//li[@class="selected"]/following-sibling::li/a/@href') \
             .get()
         if next_page:
-            yield Request(urljoin(self.base_url, next_page), callback=self.dummy_parse)
+            yield Request(urljoin(response.url, next_page), callback=self.parse)
 
     def parse_detail(self, response):
-        time.sleep(3)
-        tmp = '//th[contains(text(), {!r})]/following-sibling::td//text()'
-        cat_no = response.xpath(tmp.format('Catalogue Number:')).get()
-        batch_num = response.xpath(tmp.format('Batch Number:')).get()  # 批号
-        shipping_info = response.xpath(tmp.format('Shipping Conditions:')).get()  # 运输条件
-        controlled_drug = response.xpath(tmp.format('Controlled Drug:')).get()  # 管控产品
-        expiry_date = response.xpath(tmp.format('Expiry Date:')).get()  # 有效期
+        tmp = '//div[@class="col spd-col" and contains(text(), {!r})]/following-sibling::div//text()'
+        cat_no = response.xpath(tmp.format('Catalogue number')).get()
+        batch_num = response.xpath(tmp.format('Batch number')).get()  # 批号
+        shipping_info = response.xpath(tmp.format('Shipping conditions')).get()  # 运输条件
+        controlled_drug = response.xpath(tmp.format('Controlled drug')).get()  # 管控产品
+        expiry_date = response.xpath(tmp.format('Expiry date')).get()  # 有效期
 
         attrs = json.dumps({
             'controlled_drug': controlled_drug,
@@ -44,27 +39,27 @@ class BPSpider(BaseSpider):
             'expiry_date': expiry_date,
         })
 
-        package = response.xpath(tmp.format('Pack Size:')).get()
+        package = response.xpath(tmp.format('Pack size')).get()
         if package:
             package = package.replace(' ', '')
-        cost = response.xpath(tmp.format('Price:')).get()
+        cost = response.xpath(tmp.format('Price')).get()
         if cost:
             cost = re.search(r'(?<=£)\d+', cost).group()
 
         d = {
-            'brand': 'bp',
+            'brand': self.name,
             'cat_no': cat_no,
-            'en_name': ''.join(response.xpath('//header/h1//text()').getall()),
-            "cas": response.xpath(tmp.format('CAS Number:')).get(),
-            "info2": ''.join(response.xpath(tmp.format('Long-Term Storage')).getall()),
-            "stock_info": response.xpath(tmp.format('Availability:')).get(),
+            'en_name': ''.join(response.xpath('//h1//text()').getall()),
+            "cas": response.xpath(tmp.format('CAS number')).get(),
+            "info2": ''.join(response.xpath(tmp.format('Long term storage conditions')).getall()),
+            "stock_info": response.xpath(tmp.format('Availability')).get(),
             "prd_url": response.url,
             "shipping_info": shipping_info,
             "attrs": attrs
         }
 
         dd = {
-            "brand": 'bp',
+            "brand": self.name,
             "cat_no": cat_no,
             "package": package,
             "cost": cost,
