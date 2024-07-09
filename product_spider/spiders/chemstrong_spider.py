bee7cf4bb2828c6680bb42b7388f311a41141327
chenchunlin <chunlin.chen@cato-chem.com>
2024-07-09 16:58:41 +0800

    chemstrong 0708

---
+++ product_spider/spiders/chemstrong_spider.py
@@ -0,0 +1,42 @@
+from urllib.parse import urljoin
+
+from scrapy import Request
+
+from product_spider.items import RawData
+from product_spider.utils.functions import strip
+from product_spider.utils.spider_mixin import BaseSpider
+
+
+class ChemstrongSpider(BaseSpider):
+    name = "chemstrong"
+    brand = "chemstrong"
+    start_urls = ['https://www.qcsrm.com/index.php/Indexcn/products']
+    base_url = "https://www.qcsrm.com"
+
+    def parse(self, response, **kwargs):
+        option_nodes = response.xpath("//select[@id='jumpMenu']/option")
+        for option in option_nodes:
+            url = urljoin(self.base_url, option.xpath('./@value').get(""))
+            parent = strip(option.xpath('./text()').get())
+            yield Request(url, callback=self.parse_list, meta={"parent": parent})
+
+    def parse_list(self, response):
+        detail_urls = response.xpath("//div[@class='col-md-4 pfont']//img/parent::*/@href").getall()
+        for u in detail_urls:
+            yield Request(url=urljoin(self.base_url, u), callback=self.parse_detail, meta=response.meta)
+
+    def parse_detail(self, response):
+        img_rel = response.xpath("//div[@class='example']//img/@src").get()
+        d = {
+            'brand': self.brand,
+            'parent': response.meta.get('parent'),
+            'cat_no': response.xpath("//div[@class='col-md-4']/div/text()").getall()[0],
+            'chs_name': response.xpath("//div[@class='col-md-7 col-md-offset-1']/div/text()").get(),
+            'en_name': response.xpath("//tr/td[contains(text(),'别名:')]/parent::*/td/text()").getall()[1],
+            'cas': response.xpath("//tr/td[contains(text(),'CAS')]/parent::*/td/text()").getall()[1],
+            'mf': response.xpath("//tr/td[contains(text(),'分子式')]/parent::*/td/text()").getall()[1],
+            'mw': response.xpath("//tr/td[contains(text(),'分子量')]/parent::*/td/text()").getall()[1],
+            'img_url': img_rel and urljoin(self.base_url, img_rel),
+            'prd_url': response.url,
+        }
+        yield RawData(**d)
