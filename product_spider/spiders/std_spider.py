c888ff780f5a9217f5ed3066e9c0ed235515838d
chenchunlin <chunlin.chen@cato-chem.com>
2024-07-10 10:23:25 +0800

    0710 fixed 改为从接口获取数据

--- product_spider/spiders/std_spider.py
+++ product_spider/spiders/std_spider.py
@@ -1,11 +1,20 @@
-import re
-from string import ascii_uppercase
+import json
 from urllib.parse import urljoin
 
-from scrapy import Request
+import requests
+from scrapy import FormRequest
 
 from product_spider.items import RawData
+from product_spider.utils.functions import strip
 from product_spider.utils.spider_mixin import BaseSpider
+
+
+def get_total_page(response) -> int:
+    if response.ok:
+        data = json.loads(response.text)
+        return data['count']
+    print('获取页数失败')
+    return 0
 
 
 class STDSpider(BaseSpider):
@@ -37,3 +46,40 @@ class STDSpider(BaseSpider):
             }
             yield RawData(**d)
 
+
+    def parse_api_list(self, response):
+        data = json.loads(response.text)['data']
+        for api in data:
+            page1_response = requests.post(self.cat_url, data={
+                'page': '1',
+                'limit': '200',
+                'keyword': str(api['cat_id']),
+                'id': str(api['cat_id']),
+                'ip': ''
+            })
+            total_page = get_total_page(page1_response)
+            for i in range(0, total_page):
+                yield FormRequest(url=self.cat_url, formdata={
+                    'id': str(api['cat_id']),
+                    'keyword': str(api['cat_id']),
+                    'ip': '',
+                    'page': str(i + 1),
+                    'limit': '200',
+                }, callback=self.parse_cat_list)
+
+    def parse_cat_list(self, response):
+        data = json.loads(response.text)['data']
+        for d in data:
+            _d = {
+                'brand': self.brand,
+                'parent': strip(d['cat_cn_name']),
+                'cat_no': d['code'],
+                'chs_name': strip(d['cn_name']),
+                'en_name': strip(d['name']),
+                'cas': d['cas'],
+                'mf': d['numerator'],
+                'mw': d['molecular_weight'],
+                'img_url': urljoin(self.img_base_url, d['structure']),
+                'prd_url': f'{self.base_url}/product-detail.html?id={d["id"]}&catid={d["cat_id"]}',
+            }
+            yield RawData(**_d)
