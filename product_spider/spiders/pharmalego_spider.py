dc7dd7938af1d0551e9fb69f775dfc80183956b2
Pandaaaa906 <ye.pandaaaa906@gmail.com>
2024-07-11 17:39:47 +0800

    fix pharmalego_spider.py

--- product_spider/spiders/pharmalego_spider.py
+++ product_spider/spiders/pharmalego_spider.py
@@ -7,16 +7,6 @@ from product_spider.utils.maketrans import formula_trans
 from product_spider.utils.spider_mixin import BaseSpider
 
 
-def replace_last_slash_content(url, new_content) -> str:
-    # 找到最后一个'/'的位置
-    last_slash_index = url.rfind('/')
-    # 如果没有找到'/'，返回原URL
-    if last_slash_index == -1:
-        return url
-    # 替换最后一个'/'及之后的内容
-    return url[:last_slash_index + 1] + new_content
-
-
 class PharmalegoSpider(BaseSpider):
     name = "pharmalego"
     brand = "pharmalego"
@@ -26,8 +16,7 @@ class PharmalegoSpider(BaseSpider):
     def parse(self, response, **kwargs):
         urls = response.xpath("//div[@class='Content_list_txt']//a/@href").getall()
         for url in urls:
-            _url = urljoin(self.base_url, url)
-            yield Request(_url, callback=self.parse_list)
+            yield Request(urljoin(self.base_url, url), callback=self.parse_list)
 
     def parse_list(self, response):
         detail_urls = response.xpath("//a[@class='HProductContent_list_title']/@href").getall()
@@ -35,31 +24,39 @@ class PharmalegoSpider(BaseSpider):
             yield Request(url=urljoin(self.base_url, u), callback=self.parse_detail, meta=response.meta)
         next_href = response.xpath("(//div[@class='page']//i)[last()]/parent::*/@href").get()
         if next_href:
-            next_url = replace_last_slash_content(response.url,next_href)
-            yield Request(url=next_url, callback=self.parse_list)
+            yield Request(url=urljoin(response.url, next_href), callback=self.parse_list)
 
     def parse_detail(self, response):
         img_rel = response.xpath("//div[@class='left_top_img']//img/@src").get()
         info_xpath = "//div[@class='txt_con_list']//div[contains(text(),{!r})]/following-sibling::div[1]/text()"
+        cat_no = response.xpath(info_xpath.format('产品编号')).get()
+        cat_no = cat_no or response.xpath(info_xpath.format('产品货号')).get()
         d = {
             'brand': self.brand,
             'parent': response.xpath("(//div[contains(@class,'BreadCrumbs')]//a/text())[last()]").get(),
-            'cat_no': response.xpath(info_xpath.format('产品编号')).get(),
+            'cat_no': cat_no,
             'chs_name': response.xpath("//h1[contains(@class,'txt_title_dd')]/text()").get(),
             'en_name': response.xpath("//div[contains(@class,'txt_title_dt')]/text()").get(),
             'cas': response.xpath(info_xpath.format('CAS')).get(),
             'mf': formula_trans(response.xpath(info_xpath.format('分子式')).get()),
             'mw': response.xpath(info_xpath.format('分子量')).get(),
+            'smiles': response.xpath(info_xpath.format('Smiles Code')).get(),
             'img_url': img_rel and urljoin(self.base_url, img_rel),
             'prd_url': response.url,
         }
         yield RawData(**d)
-
-        dd = {
-            "brand": response.xpath("//div[@class='sco_content_list']/div[position()=1]/text()").get(),
-            "cat_no": d['cat_no'],
-            "package": response.xpath("//div[@class='sco_content_list']/div[position()=4]/text()").get(),
-            "cost": response.xpath("//div[@class='sco_content_list']/div[position()=5]/div/text()").get(),
-            "currency": "RMB"
-        }
-        yield ProductPackage(**dd)
+        packages = response.xpath('//div[@class="sco_content_list"]')
+        for pkg in packages:
+            brand = (brand := pkg.xpath(".//div[position()=1]/text()").get()) and brand.lower()
+            cost = pkg.xpath(".//div[position()=5]/div/text()").get()
+            cost = None if cost and '面议' in cost else cost
+            dd = {
+                "brand": brand,
+                "cat_no": d['cat_no'],
+                "package": pkg.xpath(".//div[position()=4]/text()").get(),
+                "cost": cost,
+                "price": cost,
+                "currency": "RMB"
+            }
+            if brand == self.name:
+                yield ProductPackage(**dd)
