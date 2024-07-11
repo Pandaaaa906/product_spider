6b7136bfc0bcd2f7ef7ab91acb088a08aae4c8ce
Pandaaaa906 <ye.pandaaaa906@gmail.com>
2024-07-11 16:22:33 +0800

    fix lgc_spider.py add trc full support

--- product_spider/spiders/lgc_spider.py
+++ product_spider/spiders/lgc_spider.py
@@ -1,9 +1,21 @@
 import json
+import re
+from itertools import chain
+from os import getenv
 from urllib import parse
+from urllib.parse import urljoin
+
 import scrapy
-from product_spider.items import RawData, ProductPackage
-from product_spider.utils.parsepackage import parse_package
+from scrapy import Request, FormRequest
+
+from product_spider.items import RawData, ProductPackage, SupplierProduct, RawSupplierQuotation
+from product_spider.utils.functions import strip, dumps
+from product_spider.utils.items_translate import rawdata_to_supplier_product, product_package_to_raw_supplier_quotation
+from product_spider.utils.json_path import json_nth_value, json_all_value
 from product_spider.utils.spider_mixin import JsonSpider
+
+LGC_USER = getenv('LGC_USER')
+LGC_PWD = getenv('LGC_PWD')
 
 
 def parse_brand(raw_brand):
@@ -20,12 +32,12 @@ def parse_brand(raw_brand):
 
 class LGCSpider(JsonSpider):
     name = "lgc"
-    allowd_domains = ["lgcstandards.com"]
+    allowed_domains = ["lgcstandards.com"]
     start_urls = [
         "https://www.lgcstandards.com/US/en/lgcwebservices/lgcstandards/products/search?pageSize=100&fields=FULL&sort=code-asc&currentPage=0&q=MM%3A%3Aitemtype%3ALGCProduct%3Aitemtype%3AATCCProduct&country=US&lang=en&defaultB2BUnit=",
         "https://www.lgcstandards.com/US/en/lgcwebservices/lgcstandards/products/search?pageSize=100&fields=FULL&sort=code-asc&currentPage=0&q=%3A%3Aitemtype%3ALGCProduct%3Aitemtype%3AATCCProduct&country=US&lang=en&defaultB2BUnit=",
     ]
-    base_url = "https://www.lgcstandards.com/US/en"
+    base_url = "https://www.lgcstandards.com/CA/en"
     custom_settings = {
         'CONCURRENT_REQUESTS': 3,
         'CONCURRENT_REQUESTS_PER_DOMAIN': 3,
@@ -34,7 +46,33 @@ class LGCSpider(JsonSpider):
         'AUTOTHROTTLE_START_DELAY': 5,
         'AUTOTHROTTLE_MAX_DELAY': 60,
         'AUTOTHROTTLE_TARGET_CONCURRENCY': 1,
+        'COOKIES_ENABLED': True,
+        'COOKIES_DEBUG': True,
     }
+
+    def start_requests(self):
+        # perform login
+        yield Request(self.base_url, callback=self.login)
+
+    def login(self, response):
+        csrf_token = response.xpath('//input[@name="CSRFToken"]/@value').get()
+        yield FormRequest(
+            'https://www.lgcstandards.com/CA/en/j_spring_security_check',
+            formdata={
+                "j_username": LGC_USER,
+                "j_password": LGC_PWD,
+                "CSRFToken": csrf_token,
+            },
+            callback=self.real_start_requests,
+            meta={
+                "handle_httpstatus_all": True,
+            },
+        )
+
+    def real_start_requests(self, response, post_request=None):
+        # check login status
+        # start default
+        yield from super().start_requests()
 
     def parse(self, response, **kwargs):
         products = json.loads(response.text).get("products", [])
@@ -48,22 +86,11 @@ class LGCSpider(JsonSpider):
             d = {
                 "brand": brand,
                 "cat_no": cat_no,
-                "en_name": prd.get("name", None),
-                "prd_url": prd_url,
-                "img_url": prd.get("analyteImageUrl", None),
-            }
-            dd = {
-                "brand": brand,
-                "cat_no": cat_no,
-                "currency": "USD",
             }
             yield scrapy.Request(
                 url=prd_url,
                 callback=self.parse_detail,
-                meta={
-                    "product": d,
-                    "package": dd,
-                }
+                meta={"product": d}
             )
         parsed_url = parse.urlparse(response.url)
         query_d = dict(parse.parse_qsl(parsed_url.query))
@@ -79,31 +106,82 @@ class LGCSpider(JsonSpider):
             )
 
     def parse_detail(self, response):
-        d = response.meta.get("product", None)
-        dd = response.meta.get("package", None)
+        tmpl = "//*[contains(text(), {!r})]/following-sibling::p/text()"
+        brand = json_nth_value(response.meta, '$.product.brand')
+        cat_no = json_nth_value(response.meta, '$.product.cat_no')
+        api_name = ''.join(response.xpath("//*[contains(text(), 'API Family')]/following-sibling::a/text()").getall())
+        t = response.xpath('//div[@class="product__details-left"]/script/text()').get()
+        raw_product = (m := re.search(r'var PARENT_PRODUCT = (\{.+});', t)) and m.group(1)
+        try:
+            product = json.loads(raw_product)
+        except Exception as e:
+            product = {}
+        raw_packages = (m := re.search(r'var PACK_SIZE_PRODUCTS = (\[\{.+}]);', t)) and m.group(1)
+        try:
+            packages = json.loads(raw_packages)
+        except Exception as e:
+            self.logger.warning(e)
+            packages = []
 
-        prd_attrs = json.dumps({
-            "api_name": ''.join(response.xpath("//*[contains(text(), 'API Family')]/following-sibling::a/text()").getall()),
-            "inchi": response.xpath("//*[contains(text(), 'InChI')]/following-sibling::p/text()").get(),
-            "iupac": response.xpath("//*[contains(text(), 'IUPAC')]/following-sibling::p/text()").get(),
-            "coa_urls": tuple(response.xpath('//div[@id="PdpDocumentationCoa"]//a[@class="icon download"]/@href').getall())
-        })
-        d["parent"] = ''.join(
+        coa_urls = list(chain.from_iterable(d.keys() for d in json_all_value(packages, '$[*]..coaURLs') if d))
+        categories = ''.join(
             response.xpath("//*[contains(text(), 'Product Categories')]/following-sibling::p//a/text()").getall())
-        d["info2"] = response.xpath("//*[contains(text(), 'Storage Temperature')]/following-sibling::p/text()").get()  # 储存条件
-        d["cas"] = response.xpath("//*[contains(text(), 'CAS Number')]/following-sibling::p/text()").get()
-        d["mw"] = response.xpath("//*[contains(text(), 'Molecular Weight')]/following-sibling::p/text()").get()
-        d['mf'] = response.xpath("//*[contains(text(), 'Product Format')]/following-sibling::p/text()").get()
-        d["en_name"] = response.xpath("//*[contains(text(), 'Analyte Name')]/following-sibling::p/text()").get()
-        d["shipping_info"] = response.xpath(
-            "//*[contains(text(), 'Shipping Temperature')]/following-sibling::p/text()").get()
-        d["smiles"] = response.xpath("//*[contains(text(), 'SMILES')]/following-sibling::p/text()").get()
-        d["attrs"] = prd_attrs
-        yield RawData(**d)
+        prd_attrs = {
+            "api_name": strip(api_name),
+            "inchi": strip(response.xpath(tmpl.format('InChI')).get()),
+            "iupac": strip(response.xpath(tmpl.format('IUPAC')).get()),
+            "coa_urls": coa_urls,
+            "impact_product_type": product.get('impactProductTypes')
+        }
+        img_url = response.xpath('//zoom-image/@image-src').get()
 
-        package = response.xpath("//*[contains(text(), 'Pack Size:')]/following-sibling::p/text()").get()
-        if not package:
-            return
-        dd["package"] = parse_package(package)
-        dd["stock_num"] = response.xpath("//*[@class='orderbar__stock-title in-stock-green']/text()").get()
+        d = {
+            "brand": brand,
+            "cat_no": cat_no,
+            "prd_url": response.url,
+            "img_url": img_url and urljoin(response.url, img_url),
+
+            "parent": api_name or categories,
+            "info2": response.xpath("//*[contains(text(), 'Storage Temperature')]/following-sibling::p/text()").get(),
+            # 储存条件
+            "cas": response.xpath("//*[contains(text(), 'CAS Number')]/following-sibling::p/a/text()").get(),
+            "mw": response.xpath("//*[contains(text(), 'Molecular Weight')]/following-sibling::p/text()").get(),
+            'mf': response.xpath("//*[contains(text(), 'Molecular Formula')]/following-sibling::p/text()").get(),
+            "en_name": response.xpath(tmpl.format('Analyte Name')).get(),
+            "shipping_info": response.xpath(tmpl.format('Shipping Temperature')).get(),
+            "smiles": response.xpath(tmpl.format('SMILES')).get(),
+            "attrs": dumps({k: v for k, v in prd_attrs.items() if v}),
+        }
+        if d['brand'] in {'trc', 'lgc', 'dre', 'easi-tab'}:
+            yield RawData(**d)
+        yield SupplierProduct(**rawdata_to_supplier_product(d, self.name, self.name))
+
+        for pkg in packages:
+            dd = {
+                "brand": d["brand"],
+                "cat_no": d["cat_no"],
+                "package": pkg.get('packSize'),
+                "cat_no_unit": pkg.get('code'),
+                "delivery_time": json_nth_value(pkg, '$.stock.message.title1Msg'),
+            }
+            yield Request(
+                f"https://www.lgcstandards.com/CA/en/prices?{parse.urlencode({'productCodeList': dd['cat_no_unit']})}",
+                callback=self.parse_prise,
+                meta={"pkg": dd}
+            )
+
+    def parse_prise(self, response):
+        dd = response.meta.get("pkg")
+        d = json.loads(response.text)
+        d =d.get(dd['cat_no_unit'], {})
+        cost = json_nth_value(d, f"$.price.value")
+        dd = {
+            **dd,
+            "cost": cost,
+            "price": cost,
+            "currency": json_nth_value(d, f"$.price.currencyIso"),
+        }
         yield ProductPackage(**dd)
+        if not dd['cost']:
+            return
+        yield RawSupplierQuotation(**product_package_to_raw_supplier_quotation(d, dd, self.name, dd['brand']))
