c4ec8c2cf819790548dbdcac020f8c4e5f600d41
Pandaaaa906 <ye.pandaaaa906@gmail.com>
2024-09-25 10:57:50 +0800

    fix synzeal_spider.py

--- product_spider/spiders/synzeal_spider.py
+++ product_spider/spiders/synzeal_spider.py
@@ -14,8 +14,8 @@ class SynzealSpider(BaseSpider):
     base_url = "https://www.synzeal.com"
     start_urls = map(lambda x: f"https://www.synzeal.com/category/{x}", ascii_uppercase)
 
-    def parse(self, response):
-        a_nodes = response.xpath('//a[@class="clsMaincatdiv"]')
+    def parse(self, response, **kwargs):
+        a_nodes = response.xpath('//a[contains(@class, "clsMaincatdiv")]')
         for a in a_nodes:
             parent = a.xpath('./text()').get('').strip() or None
             rel_url = a.xpath('./@href').get()
@@ -25,9 +25,9 @@ class SynzealSpider(BaseSpider):
                           headers=self.headers)
 
     def list_parse(self, response):
-        urls = response.xpath('//h4[@class="product_name"]/a/@href').extract()
+        urls = response.xpath('//h2[@class="product-title"]/a/@href').extract()
         for rel_url in urls:
-            yield Request(urljoin(self.base_url, rel_url),
+            yield Request(urljoin(response.url, rel_url),
                           callback=self.detail_parse,
                           meta=response.meta,
                           headers=self.headers)
@@ -35,19 +35,22 @@ class SynzealSpider(BaseSpider):
     def detail_parse(self, response):
         en_name = response.xpath('//h1[@class="product-detail-title"]/text()').get(default="")
         en_name = re.sub(r'\r?\n', "", en_name)
+        if not en_name:
+            en_name = response.xpath('//div[@class="product-name"]/h1/text()').get('')
         tmp = '//td[contains(text(),{!r})]/following-sibling::td/text()'
         d = {
-            'brand': "synzeal",
-            'en_name': en_name.strip(),
+            'brand': self.name,
+            'en_name': en_name,
             'prd_url': response.request.url,  # 产品详细连接
             'cat_no': response.xpath(tmp.format('SZ CAT No')).get(),
             'cas': response.xpath(tmp.format('CAS No')).get(default=""),
             'stock_info': response.xpath(tmp.format('Inv. Status')).get(),
             'mf': response.xpath(tmp.format('Mol.F.')).get(),
             'mw': response.xpath(tmp.format('Mol.Wt.')).get(),
-            'info1': response.xpath('//b[text()="Synonym: "]/../text()').get(default="").strip(),
+            'info1': response.xpath('//b[text()="Synonym: "]/../text()').get(default=""),
             'parent': response.meta.get('parent'),
             'img_url': response.xpath('//div[@class="product-details-tab"]//img/@src').get(),
+            'smiles': response.xpath('//b[text()="Smiles: "]/../text()').get(),
         }
         yield RawData(**d)
 
