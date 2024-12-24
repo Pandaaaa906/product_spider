015683cd258fdbeedf5ddea60fee35567885956e
Pandaaaa906 <ye.pandaaaa906@gmail.com>
2024-12-24 11:32:02 +0800

    improve seebio_spider.py 1. xpath里面的字串尽量用单引号，方便浏览器复制属性进去就可以用，加快开发速度 2. map_brand从if改为dict 3. 调整brand取值逻辑，宁愿插入NULL报错，不插空字符串 4. 没有复用或者二次调整的属性(chs_name/purity)，直接放到d里面 5. 调整过长的xpath(hrefs, detail_hrefs) 6. 能先yield RawData/SupplierProduct，就先yield 7. 价格为空, 不需要yield RawSupplierQuotation, 减少报错

--- product_spider/spiders/seebio_spider.py
+++ product_spider/spiders/seebio_spider.py
@@ -1,23 +1,26 @@
 import json
+from typing import Optional, Union
 
 from scrapy import Request
 
 from product_spider.items import RawData, ProductPackage, SupplierProduct, RawSupplierQuotation
+from product_spider.utils.cost import parse_cost
 from product_spider.utils.functions import strip
 from product_spider.utils.items_translate import rawdata_to_supplier_product, product_package_to_raw_supplier_quotation
 from product_spider.utils.spider_mixin import BaseSpider
 
 
-def map_brand(_brand: str) -> str:
+d_brand = {
+    "seebio": "seebio",
+    "西宝": "seebio",
+}
+
+
+def map_brand(_brand: Union[str, None]) -> Optional[str]:
     if not _brand:
-        return ""
+        return
     _brand = _brand.lower()
-    if _brand == 'seebio':
-        return _brand
-    if _brand == '西宝':
-        return 'seebio'
-    else:
-        return _brand
+    return d_brand.get(_brand, _brand)
 
 
 class SeebioSpider(BaseSpider):
@@ -42,35 +45,32 @@ class SeebioSpider(BaseSpider):
     #     return False
 
     def parse(self, response, **kwargs):
-        hrefs = response.xpath("//li[contains(@class,'drop')]//a/@href").getall()
+        hrefs = response.xpath(
+            '//li[contains(@class,"drop")]//a[contains(@href,"product_category.php")]/@href'
+        ).getall()
         for href in hrefs:
-            if href and 'www.seebio.cn/product_category.php' in href:
-                yield Request(href, callback=self.parse_list)
+            yield Request(href, callback=self.parse_list)
 
     def parse_list(self, response):
-        detail_hrefs = response.xpath(
-            "//div[@class='entry-summary post-summary']/div[@class='entry-content']/p[@class='d-desc']/a/@href").getall()
+        detail_hrefs = response.xpath('//h2[@class="entry-title post-title"]/a/@href').getall()
         for href in detail_hrefs:
-            if href and 'www.seebio.cn/product.php' in href:
-                yield Request(href, callback=self.parse_detail)
+            yield Request(href, callback=self.parse_detail)
 
-        next_page = response.xpath("//div[@id='pages']/div/a[contains(text(),'下一页')]/@href").get()
-        last_page = response.xpath("//div[@id='pages']/div/a[contains(text(),'最后一页')]/@href").get()
+        next_page = response.xpath('//div[@id="pages"]/div/a[contains(text(),"下一页")]/@href').get()
+        last_page = response.xpath('//div[@id="pages"]/div/a[contains(text(),"最后一页")]/@href').get()
         if next_page and next_page != last_page:
             yield Request(next_page, callback=self.parse_list)
 
     def parse_detail(self, response):
-        parent = strip(response.xpath("//div[@class='col-md-9 col-sm-8 location']/span/a[last()]/text()").get(), '')
-        if parent in ['首页', '产品中心']:
+        tmp = '//div[@class="product-info"]/ul/li[contains(text(),{!r})]/text()'
+        tmp2 = '//table[@id="baseinfo"]//tr/td[contains(text(),{!r})]/following-sibling::*[1]//text()'
+
+        parent = strip(response.xpath('//div[@class="col-md-9 col-sm-8 location"]/span/a[last()]/text()').get())
+        if parent in {'首页', '产品中心'}:
             parent = None
 
-        tmp = "//div[@class='product-info']/ul/li[contains(text(),{!r})]/text()"
         cat_no = strip(response.xpath(tmp.format("编码")).get(), "").strip("编码：")
         introduction = strip(response.xpath(tmp.format("简介")).get(), "").strip("简介：")
-        purity = strip(response.xpath(tmp.format("级别")).get(), "").strip("级别：")
-        chs_name = response.xpath("//div[@class='product-info']/h1/text()").get()
-
-        tmp2 = "//table[@id='baseinfo']//tr/td[contains(text(),{!r})]/following-sibling::*[1]//text()"
 
         if not (en_name := response.xpath(tmp2.format("英文名")).get()):
             # 如果基本信息中没有英文名 尝试从上面的别名获取
@@ -78,13 +78,18 @@ class SeebioSpider(BaseSpider):
                 if _[0].isalnum():
                     en_name = _
 
-        cas = response.xpath(tmp2.format("CAS")).get()
+        table_heads = response.xpath('//table[@id="packlist"]/thead/tr/th/text()').getall()
+        brand_index = table_heads.index("品牌")
+        package_index = table_heads.index("包装规格")
+        stock_index = table_heads.index("库存")
+        price_index = table_heads.index("价格")
+
         if not (brand := response.xpath(tmp2.format("品牌")).get()):
-            brand = self.brand
+            brand = response.xpath(f'//table[@id="packlist"]//tr/td[{brand_index+1}]/text()').get()
         brand = map_brand(brand)
 
-        if not (img_url := response.xpath("//div[@id='product']//div[@class='product-img']//a/@href").get()):
-            img_url = response.xpath("//div[@class='swiper-slide']/img/@src").get()
+        if not (img_url := response.xpath('//div[@id="product"]//div[@class="product-img"]//a/@href').get()):
+            img_url = response.xpath('//div[@class="swiper-slide"]/img/@src').get()
 
         attrs = {
             'product_info': introduction,
@@ -92,32 +97,30 @@ class SeebioSpider(BaseSpider):
         d = {
             'brand': brand,
             'cat_no': cat_no,
-            'chs_name': chs_name,
+            'chs_name': response.xpath('//div[@class="product-info"]/h1/text()').get(),
             'en_name': en_name,
-            'cas': cas,
+            'cas': response.xpath(tmp2.format("CAS")).get(),
             'parent': parent,
-            'purity': purity,
+            'purity': strip(response.xpath(tmp.format("级别")).get(), "").strip("级别："),
             'img_url': img_url,
             'prd_url': response.url,
             'attrs': json.dumps(attrs, ensure_ascii=False),
         }
 
-        table_heads: list = response.xpath("//table[@id='packlist']/thead/tr/th/text()").getall()
-        package_index = table_heads.index("包装规格")
-        stock_index = table_heads.index("库存")
-        price_index = table_heads.index("价格")
+        ddd = rawdata_to_supplier_product(d, platform=self.name, vendor=self.name)
+        yield SupplierProduct(**ddd)
+        yield RawData(**d)
 
-        package_rows = response.xpath("//table[@id='packlist']/tbody/tr")
+        package_rows = response.xpath('//table[@id="packlist"]/tbody/tr')
         for row in package_rows:
             if not (tds := row.xpath('./td')) or len(tds) <= max(stock_index, price_index, package_index):
                 self.logger.warning(f"规格信息异常 prd_url:{response.url}")
                 continue
             stock_num = tds[stock_index].xpath("./text()").get()
             package = tds[package_index].xpath("./text()").get()
-            price = None
-            if _ := tds[price_index].xpath("./em/text()").get():
-                price = _.strip("元").replace(",", "")
-
+            price = parse_cost(tds[price_index].xpath("./em/text()").get())
+            if price == '面议':
+                price = None
             dd = {
                 'brand': brand,
                 'cat_no': cat_no,
@@ -129,8 +132,6 @@ class SeebioSpider(BaseSpider):
             }
             dddd = product_package_to_raw_supplier_quotation(d, dd, platform=self.name, vendor=self.name)
             yield ProductPackage(**dd)
-            yield RawSupplierQuotation(**dddd)
+            if price:
+                yield RawSupplierQuotation(**dddd)
 
-        ddd = rawdata_to_supplier_product(d, platform=self.name, vendor=self.name)
-        yield SupplierProduct(**ddd)
-        yield RawData(**d)
