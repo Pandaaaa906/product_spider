3fd8478007480165ded81a4db1e4d173f78a71e7
chenchunlin <chunlin.chen@cato-chem.com>
2025-07-23 12:03:26 +0800

    fix purechemland: start from https://purechemland.shop/

--- product_spider/spiders/purechemland_spider.py
+++ product_spider/spiders/purechemland_spider.py
@@ -8,7 +8,7 @@ from product_spider.utils.spider_mixin import BaseSpider
 
 class PurechemlandSpider(BaseSpider):
     name = "purechemland"
-    start_urls = ["https://purechemland.com/", ]
+    start_urls = ["https://purechemland.shop/", ]
     base_url = "https://purechemland.com/"
     brand = 'purechemland'
     custom_settings = {
@@ -30,54 +30,121 @@ class PurechemlandSpider(BaseSpider):
     currency = 'RMB'
 
     def parse(self, response, **kwargs):
-        rel_urls = response.xpath("//ul/li/a[contains(@href,'/product_types/')]/@href").getall()
-        for rel_url in rel_urls:
+        rel_urls = response.xpath("//ul[@id='subNav']//a[contains(@href,'/product_categories/')]/@href").getall()
+        for rel_url in set(rel_urls):
             url = get_url(response.url, rel_url)
             yield Request(url, self.parse_list)
 
     def parse_list(self, response):
-        product_trs = response.xpath("//div[@class='table_form']/table/tbody/tr")
-        parent = response.xpath("//div[@class='txt']/h3//text()").get()
+        product_urls = response.xpath("//tbody[@id='goodList']/tr/td/a/@href").getall()
+        parent = response.xpath("//span[@id='titlename']/text()").get()
+        for product_url in set(product_urls):
+            product_url = get_url(response.url, product_url)
+            yield Request(product_url, self.parse_detail, meta={'parent': parent})
 
-        headers = response.xpath("//div[@class='table_form']/table/thead//th/text()").getall()
+        next_url = response.xpath("//a[@rel='next']/@href").get()
+        if next_url:
+            next_url = get_url(response.url, next_url)
+            yield Request(next_url, self.parse_list)
+
+    def parse_detail(self, response):
+        brand = response.xpath("//dt[contains(text(),'品牌')]/following-sibling::*[1]//text()").get()
+        if brand and 'PCL' not in brand.upper():
+            return
+        cat_no = response.xpath("//dt[contains(text(),'产品编号')]/following-sibling::*[1]//text()").get()
+        if not cat_no:
+            good_info_list = response.xpath("//div[@class='goods-info-list']")
+            if not good_info_list:
+                return
+            else:
+                self.logger.info('Call parse_detail2')
+                yield from self.parse_detail2(response)
+                return
+        cas = response.xpath("//dt[contains(text(),'CAS')]/following-sibling::*[1]//text()").get(),
+        if cas and 'N/A' in cas:
+            cas = None
+        img_url = response.xpath("//img[@id='current-img']/@src").get()
+        if img_url:
+            if '/store/img/default.png' in img_url:
+                img_url = None
+            else:
+                img_url = get_url(response.url, img_url)
+
+        cat_no = cat_no.replace('PCL-#-', '')
+        d = {
+            "brand": self.brand,
+            "parent": response.meta.get("parent"),
+            "cat_no": cat_no,
+            "chs_name": response.xpath("//h1[@class='tit']/text()").get(),
+            "en_name": response.xpath("//p[@class='subtitle']/text()").get(),
+            "cas": cas,
+            "prd_url": response.url,
+            "img_url": img_url,
+            'purity': response.xpath("//dt[contains(text(),'产品特性描述')]/following-sibling::*[1]//text()").get()
+        }
+
+        yield RawData(**d)
+        price = response.xpath("//*[@id='price']//text()").get()
+        if price:
+            price = price.replace("¥", '')
+        package = response.xpath("//dt[contains(text(),'包装')]/following-sibling::*[1]//text()").get()
+        dd = {
+            "brand": self.name,
+            "cat_no": d['cat_no'],
+            "package": package,
+            "cost": price,
+            "price": price,
+            "currency": self.currency,
+            'purity': d['purity'],
+            'stock_num': response.xpath("//dt[contains(text(),'库存')]/following-sibling::*[1]//text()").get(),
+        }
+        yield ProductPackage(**dd)
+
+    # 适配第二种布局 https://purechemland.shop/products/40557
+    def parse_detail2(self, response):
+        product_trs = response.xpath("//div[@class='goods-info-list']/table/tbody/tr")
+        headers = response.xpath("//div[@class='goods-info-list']/table/thead//th/text()").getall()
         headers = [x.strip() for x in headers]
         property_map = {}
         for index, th in enumerate(headers):
             property_map[th] = index
 
-        if not product_trs:
-            return
+        chs_name = response.xpath("//h1[@class='tit']/text()").get()
+        en_name = response.xpath("//p[@class='subtitle']/text()").get()
+        cas = response.xpath("//dt[contains(text(),'CAS')]/following-sibling::*[1]//text()").get()
+        img_url = response.xpath("//img[@id='current-img']/@src").get()
+        if img_url:
+            if '/store/img/default.png' in img_url:
+                img_url = None
+            else:
+                img_url = get_url(response.url, img_url)
+        if cas and 'N/A' in cas:
+            cas = None
         for tr in product_trs:
             tds = tr.xpath('./td')
-            cat_no: str = tds[property_map.get('货号')].xpath('.//text()').get()
-
-            if cat_no:
-                cat_no = cat_no.replace('PCL-#-', '')
-            else:
-                self.logger.warning('Cat No Not found!')
-                continue
             d = {
                 "brand": self.brand,
-                "parent": parent,
-                "cat_no": cat_no,
-                "chs_name": f'{tds[property_map.get("产品名称")].xpath('.//text()').get()} {tds[property_map.get("产品描述")].xpath('.//text()').get()}',
-                "cas": tds[property_map.get('CAS号')].xpath('.//text()').get(),
-                'prd_url': response.url,
+                "parent": response.meta.get("parent"),
+                "cat_no": tds[property_map.get('货号')].xpath('.//text()').get(),
+                "chs_name": chs_name,
+                "en_name": en_name,
+                "cas": cas,
+                "prd_url": response.url,
+                "img_url": img_url,
+                'purity': tds[property_map.get('规格型号')].xpath('.//text()').get(),
             }
             yield RawData(**d)
-            package = tds[property_map.get('规格')].xpath('.//text()').get()
-            if package:
-                packages = package.split(',')
-                for p in packages:
-                    dd = {
-                        "brand": self.brand,
-                        "cat_no": d['cat_no'],
-                        "package": p,
-                        "currency": self.currency,
-                    }
-                    yield ProductPackage(**dd)
-
-        next_url = response.xpath("//li[@class='pageItem pageItemActive']/following-sibling::li[1]/a/@href").get()
-        if next_url:
-            next_url = get_url(response.url, next_url)
-            yield Request(next_url, self.parse_list)
+            price = tds[property_map.get('目录价')].xpath('./span/text()').get()
+            if price:
+                price = price.replace("¥", '')
+            dd = {
+                "brand": self.brand,
+                "cat_no": d['cat_no'],
+                "package": tds[property_map.get('参数说明')].xpath('.//text()').get(),
+                'price': price,
+                'cost': price,
+                "currency": 'RMB',
+                'purity': tds[property_map.get('规格型号')].xpath('.//text()').get(),
+                'delivery_time': tds[property_map.get('货期')].xpath('.//text()').get(),
+            }
+            yield ProductPackage(**dd)
