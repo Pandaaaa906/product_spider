ebd4c5a2ac1022b89a22190b829bdd423b7355ba
chenchunlin <chunlin.chen@cato-chem.com>
2025-10-28 13:34:05 +0800

    fix synquestlabs_spider: purity

--- product_spider/spiders/synquestlabs_spider.py
+++ product_spider/spiders/synquestlabs_spider.py
@@ -72,7 +72,7 @@ class SynquestlabsSpider(BaseSpider):
             "en_name": response.xpath("//div[contains(@class,'product-details')]/h2/text()").get(),
             "cas": cas,
             "mf": response.xpath("//td[text()='Molecular Formula']/following-sibling::td[1]/span/text()").get(),
-            "purity": response.xpath("//td[text()='Molecular Formula']/following-sibling::td[1]/span/text()").get(),
+            "purity": response.xpath("//td[text()='Purity']/following-sibling::td[1]/span/text()").get(),
             "mdl": response.xpath("//td[text()='MDL Number']/following-sibling::td[1]/span/text()").get(),
             "mw": response.xpath("//td[text()='Molecular Weight']/following-sibling::td[1]/span/text()").get(),
             "img_url": img_url,
