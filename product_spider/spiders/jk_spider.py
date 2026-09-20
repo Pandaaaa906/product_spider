6a7fbf0e3d24d3d49774e606f16c3f5d5092bcfa
Pandaaaa906 <ye.pandaaaa906@gmail.com>
2026-09-20 17:11:42 +0800

    fix jk_spider.py

--- product_spider/spiders/jk_spider.py
+++ product_spider/spiders/jk_spider.py
@@ -2,14 +2,20 @@ import json
 import re
 from urllib.parse import urlencode, quote
 
-import scrapy
 from jsonpath_ng import parse
 from scrapy import Request
-from scrapy.http import JsonRequest
 
-from product_spider.items import RawData, SupplierProduct, ProductPackage, RawSupplierQuotation
-from product_spider.utils.functions import dumps, first
-from product_spider.utils.items_translate import rawdata_to_supplier_product, product_package_to_raw_supplier_quotation
+from product_spider.items import (
+    RawData,
+    SupplierProduct,
+    ProductPackage,
+    RawSupplierQuotation,
+)
+from product_spider.utils.functions import dumps
+from product_spider.utils.items_translate import (
+    rawdata_to_supplier_product,
+    product_package_to_raw_supplier_quotation,
+)
 from product_spider.utils.spider_mixin import BaseSpider
 
 jk_brands = {"jk"}
@@ -48,34 +54,60 @@ class JkPrdSpider(BaseSpider):
     name = "jk"
     allowed_domains = ["jkchemical.com"]
     base_url = "http://www.jkchemical.com"
-    product_url = 'https://www.jkchemical.com/_next/data/o4gC8HmZ5IFTLEhskqrzE/search/{catalog}.json'
-    catalog_url = 'https://www.jkchemical.com/_next/data/o4gC8HmZ5IFTLEhskqrzE/product-catalog/{catalog_id}.json'
+    home_url = "https://www.jkchemical.com/"
+    seed_catalog_id = 1013  # 任意存在的分类，其 JSON 内嵌完整目录树
+    product_url = (
+        "https://www.jkchemical.com/_next/data/{build_id}/search/{catalog}.json"
+    )
+    catalog_url = "https://www.jkchemical.com/_next/data/{build_id}/product-catalog/{catalog_id}.json"
 
     custom_settings = {
-        "DOWNLOADER_MIDDLEWARES": {
-            'product_spider.middlewares.proxy_middlewares.RandomProxyMiddleWare': 543,
-        },
-        'PROXY_POOL_REFRESH_STATUS_CODES': [503, 504, 429],
-        'RETRY_TIMES': 10,
-        'DEFAULT_REQUEST_HEADERS': {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) '
-                          'AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8',
-            # hardcoding?
-            'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MCwidW5pdCI6MjQsImd1ZXN0Ijo3NjE2OTUsInVxIjo1'
-                             'NCwicm9sZXMiOm51bGwsImlhdCI6MTYyNzk2Njk4NX0.8mea_0U6wOZKvqrb-y6k689j8R1coOcnSUNOIOHyiMo',
+        "PROXY_POOL_REFRESH_STATUS_CODES": [503, 504, 429],
+        "RETRY_TIMES": 10,
+        "CONCURRENT_REQUESTS": 2,
+        "CONCURRENT_REQUESTS_PER_DOMAIN": 2,
+        "CONCURRENT_REQUESTS_PER_IP": 2,
+        "DOWNLOAD_DELAY": 2,
+        "DEFAULT_REQUEST_HEADERS": {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) "
+            "AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8",
         },
     }
 
     def _start_requests(self):
-        d = {
-            'language': 196,
-            'salesRegion': 1,
-        }
-        yield JsonRequest(
-            'https://shop.jkchemical.com/uq/prod/product/tv/query/GetRootCategory',
-            data=d,
-            callback=self.parse
+        yield Request(self.home_url, callback=self.parse_home)
+
+    def parse_home(self, response):
+        """从首页提取 Next.js buildId，再请求目录树 JSON"""
+        m = re.search(r'"buildId":"([^"]+)"', response.text)
+        if not m:
+            raise ValueError("无法从首页提取 buildId，网站可能再次改版")
+        self.build_id = m.group(1)
+        yield Request(
+            self.catalog_url.format(
+                build_id=self.build_id, catalog_id=self.seed_catalog_id
+            ),
+            callback=self.parse_catalogs,
         )
+
+    def parse_catalogs(self, response):
+        """目录树内嵌在任意 catalog JSON 的 rootProductCatalogs，遍历叶子节点"""
+        obj = response.json()
+        seen = set()
+
+        def walk(node):
+            catalog_id = node.get("id")
+            children = node.get("children") or []
+            if node.get("isLeaf") or not children:
+                if catalog_id not in seen:
+                    seen.add(catalog_id)
+                    yield self.make_request(catalog_id, node.get("name"))
+            else:
+                for child in children:
+                    yield from walk(child)
+
+        for m in parse("$.pageProps.rootProductCatalogs[*]").find(obj):
+            yield from walk(m.value)
 
     def make_request(self, catalog_id, catalog_name, page: int = 1):
         d = {
@@ -85,100 +117,84 @@ class JkPrdSpider(BaseSpider):
             "categoryId": catalog_id,
         }
         return Request(
-            f"{self.product_url.format(catalog=quote(catalog_name))}?{urlencode(d)}",
-            meta={'page': page, 'catalog_id': catalog_id, 'catalog_name': catalog_name},
-            callback=self.parse_list
+            f"{self.product_url.format(build_id=self.build_id, catalog=quote(catalog_name))}?{urlencode(d)}",
+            meta={"page": page, "catalog_id": catalog_id, "catalog_name": catalog_name},
+            callback=self.parse_list,
         )
-
-    def catalog_request(self, catalog_id, catalog_name):
-        return Request(
-            self.catalog_url.format(catalog_id=catalog_id),
-            self.parse_catalog,
-            meta={"catalog_id": catalog_id, "catalog_name": catalog_name}
-        )
-
-    def parse(self, response, **kwargs):
-        obj = response.json()
-        ret = obj.get('res', '')
-        for line in ret.split('\n'):
-            m = re.match(r"(\d+)\t(\d+)\t(\S+)", line)
-            if not m:
-                continue
-            catalog_id, _, catalog_name, *_ = m.groups()
-            yield self.catalog_request(catalog_id, catalog_name=catalog_name)
-
-    def parse_catalog(self, response):
-        obj = response.json()
-        if first(parse('$..__N_REDIRECT').find(obj), None):
-            catalog_id = response.meta.get("catalog_id")
-            catalog_name = response.meta.get("catalog_name")
-            yield self.make_request(catalog_id, catalog_name)
-            return
-        catalogs = parse('$..currCatalog.children[*]').find(obj)
-        for m in catalogs:
-            catalog = m.value
-            yield self.catalog_request(catalog_id=catalog.get("id"), catalog_name=catalog.get("name"))
 
     def parse_list(self, response):
         obj = response.json()
-        prds = parse('$..productlist[*]').find(obj)
-        for item in prds:
-            if not item or not (t := item.value) or not (prd := t.get("props")):
-                continue
-            mf = prd.get('molecularFomula')
+        page_props = obj.get("pageProps") or {}
+        prds = page_props.get("productlist") or []
+        for prd in prds:
+            mf = prd.get("molecularFomula")
+            cas = prd.get("cas")
             d = {
-                'brand': parse_brand(prd.get('brandName')),
-                'cat_no': prd.get('origin'),
-                'en_name': prd.get('englishName'),
-                'chs_name': prd.get('chineseName'),
-                'cas': cas if (cas := prd.get('cas')) != '0' else None,
-                'purity': prd.get('purity'),
-                'mf': mf.replace(" ", "") if mf else None,
-                'mw': prd.get('molecularWeight'),
-                'img_url': (img_url_id := prd.get(
-                    'imageUrl')) and f'https://static.jkchemical.com/Structure/{img_url_id[:3]}/{img_url_id}.png',
-                'prd_url': (tmp := prd.get('id')) and f'https://www.jkchemical.com/product/{tmp}'
+                "brand": parse_brand(prd.get("brandName")),
+                "cat_no": prd.get("origin"),
+                "en_name": prd.get("englishName"),
+                "chs_name": prd.get("chineseName"),
+                "cas": None if cas == "0" else cas,
+                "purity": prd.get("purity"),
+                "mf": mf.replace(" ", "") if mf else None,
+                "mw": prd.get("molecularWeight"),
+                "img_url": (img_url_id := prd.get("imageUrl"))
+                and f"https://static.jkchemical.com/Structure/{img_url_id[:3]}/{img_url_id}.png",
+                "prd_url": (tmp := prd.get("id"))
+                and f"https://www.jkchemical.com/product/{tmp}",
             }
 
-            yield SupplierProduct(**rawdata_to_supplier_product(
-                d,
-                platform='jk',
-                vendor='jk',
-            ))
-            if not (prd_url := d['prd_url']):
+            yield SupplierProduct(
+                **rawdata_to_supplier_product(
+                    d,
+                    platform="jk",
+                    vendor="jk",
+                )
+            )
+            if not (prd_url := d["prd_url"]):
                 continue
             yield Request(prd_url, callback=self.parse_package, meta={"prd": d})
 
-        if not prds:
-            return
-
-        cur_page = response.meta.get("page")
-        catalog_id = response.meta.get("catalog_id")
-        catalog_name = response.meta.get("catalog_name")
-        yield self.make_request(catalog_id, catalog_name, cur_page + 1)
+        total = page_props.get("total") or 0
+        page_index = page_props.get("pageIndex") or response.meta.get("page", 1)
+        page_size = page_props.get("pageSize") or 20
+        if page_index * page_size < total:
+            yield self.make_request(
+                response.meta.get("catalog_id"),
+                response.meta.get("catalog_name"),
+                page_index + 1,
+            )
 
     def parse_package(self, response):
         d = response.meta.get("prd", {})
-        cat_nodes = response.xpath('//div[./div/text()="产品分类"]/following-sibling::div[1]/div')
-        categories = [
-            "__".join(node.xpath('./span/a/text()').getall())
-            for node in cat_nodes
-        ]
+        raw_json = response.xpath('//script[@id="__NEXT_DATA__"]/text()').get()
+        j_obj = json.loads(raw_json) if raw_json else None
+
+        categories = []
+        if j_obj:
+            categories = [
+                "__".join(node.get("name") or "" for node in m.value)
+                for m in parse("$.props.pageProps.product.productCrumbs[*]").find(j_obj)
+            ]
         attrs = {}
         if categories:
             attrs["categories"] = categories
             d["parent"] = categories[0]
-        d['attrs'] = dumps(attrs)
-        if d["brand"] in jk_brands:
+        d["attrs"] = dumps(attrs)
+        # JSON 缺失时仍保留产品记录（无分类）
+        if d.get("brand") in jk_brands:
             yield RawData(**d)
-        raw_json = response.xpath('//script[@id="__NEXT_DATA__"]/text()').get()
-        if not raw_json:
+        if not j_obj:
             return
-        j_obj = json.loads(raw_json)
-        packages = parse('$..product.packages[*]').find(j_obj)
-        for m in packages:
+        for m in parse("$.props.pageProps.product.packages[*]").find(j_obj):
             pkg = m.value
             package = f"{pkg.get('radioY')}{pkg.get('unit')}"
+            stock_num = 0
+            if l_inv := pkg.get("inventories"):
+                for inv in l_inv:
+                    stock_num += inv.get("quantity", 0)
+            delivery_time = "现货" if stock_num > 0 else None
+
             dd = {
                 "brand": d.get("brand"),
                 "cat_no": d.get("cat_no"),
@@ -186,12 +202,18 @@ class JkPrdSpider(BaseSpider):
                 "cost": pkg.get("salesPrice"),
                 "price": pkg.get("price"),
                 "currency": pkg.get("currency"),
-                "attrs": dumps({
-                    "inventories": pkg.get("inventories")
-                })
+                "stock_num": stock_num,
+                "delivery_time": delivery_time,
+                "attrs": dumps({"inventories": pkg.get("inventories")}),
             }
             if dd["brand"] in jk_brands:
                 yield ProductPackage(**dd)
-            yield RawSupplierQuotation(**product_package_to_raw_supplier_quotation(
-                d, dd, "jk", "jk",
-            ))
+            if dd["cost"]:
+                yield RawSupplierQuotation(
+                    **product_package_to_raw_supplier_quotation(
+                        d,
+                        dd,
+                        self.name,
+                        self.name,
+                    )
+                )
